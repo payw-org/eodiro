d8b5bccf08e4323da7c3cb51f7f69d502b1ff1da
Jang Haemin <io@jhaemin.com>
2019-03-30 12:49:43 +0900

    New building selection component

---
+++ src/pages/Buildings.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as Components from '../components'
+
+export default class Buildings extends React.Component {
+	render() {
+		return (
+			<div id="buildings">
+				<Components.AppNav title="Choose a building" />
+			</div>
+		)
+	}
+}
