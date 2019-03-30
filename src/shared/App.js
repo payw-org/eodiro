df99053609b76c4f9c6a4314ab396f80434220ee
Jang Haemin <io@jhaemin.com>
2019-03-30 12:48:13 +0900

    Add routes

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,11 +1,16 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import * as Pages from '../pages'
 
 export default class App extends Component {
 	render() {
 		return (
-			<div>
-
+			<div id="eodiro-app">
+				<Switch>
+					<Route exact path="/" component={Pages.Home} />
+					<Route exact path="/buildings" component={Pages.Buildings} />
+					<Route exact component={Pages.NotFound} />
+				</Switch>
 			</div>
 		)
 	}
