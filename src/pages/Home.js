b00f1dacb331d9df726125880b79d8c96a62ac70
Jang Haemin <io@jhaemin.com>
2019-03-30 23:02:15 +0900

    Add transition

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,42 @@
 import React from 'react'
 import HomeBGTile from '../components/HomeBGTile'
 import { Link } from 'react-router-dom'
+import { CSSTransition } from 'react-transition-group'
+
 import '../scss/home'
 
 export default class Home extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			goBoxMounted: false
+		}
+	}
+
+	componentDidMount() {
+		this.setState({
+			goBoxMounted: !this.state.goBoxMounted
+		})
+	}
+
 	render() {
 		return (
-			<div id="home">
+			<div id="home">				
 				<HomeBGTile />
-				<div className="go-box">
-					<div>
-						<img className="logo" src="/assets/images/eodiro/logo.svg" alt=""/>
-						<Link to="/buildings"><button className="go-btn">GO</button></Link>
+				<CSSTransition
+					in={this.state.goBoxMounted}
+					appear
+					unmountOnExit
+					timeout={1500}
+					classNames="trans"
+				>
+					<div className="go-box">
+						<div>
+							<img className="logo" src="/assets/images/eodiro/logo.svg" alt=""/>
+							<Link to="/buildings"><button className="go-btn">GO</button></Link>
+						</div>
 					</div>
-				</div>
+				</CSSTransition>
 			</div>
 		)
 	}
