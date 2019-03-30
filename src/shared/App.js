b9fc82449636c5b4f56d7884fcfcfabc1250cd93
Jang Haemin <io@jhaemin.com>
2019-03-30 23:17:44 +0900

    Replace with Container component

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,17 +1,10 @@
-import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
-import * as Pages from '../pages'
+import React from 'react'
+import Container from '../components/Container'
 
-export default class App extends Component {
+export default class App extends React.Component {
 	render() {
 		return (
-			<div id="eodiro-app">
-				<Switch>
-					<Route exact path="/" component={Pages.Home} />
-					<Route exact path="/buildings" component={Pages.Buildings} />
-					<Route exact component={Pages.NotFound} />
-				</Switch>
-			</div>
+			<Container />
 		)
 	}
 }
