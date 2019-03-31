ff7943359312b16c1da3302658cb9ad77f0415b2
Jang Haemin <io@jhaemin.com>
2019-03-31 12:46:16 +0900

    Add routes in route

--- src/pages/Buildings.js
+++ src/pages/Buildings.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import * as Components from '../components'
+import { Route, Switch, Link } from 'react-router-dom'
+import { TransitionGroup, CSSTransition } from "react-transition-group"
+
+import '../scss/buildings'
 
 export default class Buildings extends React.Component {
 	constructor(props) {
@@ -22,6 +26,10 @@ export default class Buildings extends React.Component {
 		return (
 			<div id="buildings">
 				<Components.AppNav title="Choose a building" willExit={!this.state.mounted} />
+				<Switch>
+					<Route path={`${this.props.match.url}/:buildingId`} render={() => (<h3><Link to="/buildings">back to buildings</Link></h3>)} />
+					<Route path={this.props.match.url} render={() => (<h3><Link to="/buildings/310">go to 310</Link></h3>)} />
+				</Switch>
 			</div>
 		)
 	}
