23eb05c42fa17445c1f1b7f414923ba44970e2e8
Jang Haemin <io@jhaemin.com>
2019-03-31 12:46:05 +0900

    Remove page transition

--- src/components/Container.js
+++ src/components/Container.js
@@ -7,38 +7,36 @@ function Container({ location }) {
 	let willExit = false
 
 	return (
-		<TransitionGroup>
-			<CSSTransition
-				key={location.key}
-				timeout={{ enter: 0, exit: 500 }}
-				onExit={() => {
-					willExit = true
-				}}
-				onExited={() => {
-					willExit = false
-				}}
-				classNames="trans"
-			>
+		// <TransitionGroup className="eodiro-app">
+		// 	<CSSTransition
+		// 		key={location.key}
+		// 		timeout={{ enter: 0, exit: 500 }}
+		// 		onExit={() => {
+		// 			willExit = true
+		// 		}}
+		// 		onExited={() => {
+		// 			willExit = false
+		// 		}}
+		// 		classNames="fade"
+		// 	>
 				<div className="eodiro-app">
 					<Switch location={location}>
 						<Route
 							exact path="/"
-							// component={Pages.Home}
 							render={(props) => <Pages.Home {...props} willExit={willExit} />}
 						/>
 						<Route
-							exact path="/buildings"
-							// component={Pages.Buildings}
+							path="/buildings"
 							render={(props) => <Pages.Buildings {...props} willExit={willExit} />}
 						/>
 						<Route
 							exact
-							// component={Pages.NotFound}
+							component={Pages.NotFound}
 						/>
 					</Switch>
 				</div>
-			</CSSTransition>
-		</TransitionGroup>
+		// 	</CSSTransition>
+		// </TransitionGroup>
 	)
 }
 
