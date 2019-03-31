f33f27a539356240a0423f440f907814c1ca2639
Jang Haemin <io@jhaemin.com>
2019-03-31 17:09:56 +0900

    Convert 2 spaces from a single tab

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,40 +4,40 @@ import { TransitionGroup, CSSTransition } from "react-transition-group"
 import * as Pages from '../pages'
 
 function Container({ location }) {
-	let willExit = false
+  let willExit = false
 
-	return (
-		// <TransitionGroup className="eodiro-app">
-		// 	<CSSTransition
-		// 		key={location.key}
-		// 		timeout={{ enter: 0, exit: 500 }}
-		// 		onExit={() => {
-		// 			willExit = true
-		// 		}}
-		// 		onExited={() => {
-		// 			willExit = false
-		// 		}}
-		// 		classNames="fade"
-		// 	>
-				<div className="eodiro-app">
-					<Switch location={location}>
-						<Route
-							exact path="/"
-							render={(props) => <Pages.Home {...props} willExit={willExit} />}
-						/>
-						<Route
-							path="/buildings"
-							render={(props) => <Pages.Buildings {...props} willExit={willExit} />}
-						/>
-						<Route
-							exact
-							component={Pages.NotFound}
-						/>
-					</Switch>
-				</div>
-		// 	</CSSTransition>
-		// </TransitionGroup>
-	)
+  return (
+    // <TransitionGroup className="eodiro-app">
+      <CSSTransition
+        key={location.key}
+        timeout={{ enter: 0, exit: 500 }}
+        onExit={() => {
+          willExit = true
+        }}
+        onExited={() => {
+          willExit = false
+        }}
+        classNames="fade"
+      >
+        <div className="eodiro-app">
+          <Switch location={location}>
+            <Route
+              exact path="/"
+              render={(props) => <Pages.Home {...props} willExit={willExit} />}
+            />
+            <Route
+              path="/buildings"
+              render={(props) => <Pages.Buildings {...props} willExit={willExit} />}
+            />
+            <Route
+              exact
+              component={Pages.NotFound}
+            />
+          </Switch>
+        </div>
+      </CSSTransition>
+    // </TransitionGroup>
+  )
 }
 
 export default withRouter(Container)
