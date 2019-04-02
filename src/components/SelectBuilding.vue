b18dc87a4e126981b028bf8bbad4d5b3198fab26
Jang Haemin <io@jhaemin.com>
2019-04-03 00:56:06 +0900

    Update

--- src/components/SelectBuilding.vue
+++ src/components/SelectBuilding.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="content-item select-building">
-    <h1>Select building</h1>
     <router-link to="/buildings/310">310관</router-link>
   </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+
+</style>
