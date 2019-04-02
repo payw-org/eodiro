fe7158087fe9d89af16018bd8c3098822b73e3e9
Jang Haemin <io@jhaemin.com>
2019-04-02 14:18:30 +0900

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,6 @@
 <template>
   <div id="app">
     <transition
-      appear
-      name="fade"
-    >
-    <h1>Hello App!</h1>
-    </transition>
-    <router-link to="/">Go to home</router-link>
-    <router-link to="/buildings">Go to buildings</router-link>
-
-    <transition
-      appear
       name="fade"
     >
       <router-view></router-view>
@@ -22,6 +12,8 @@
 import './scss/globalstyle'
 
 export default {
-  name: 'app'
+  name: 'App',
+  watch: {
+  }
 }
 </script>
