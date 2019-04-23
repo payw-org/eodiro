7399ae926e36a528f5bc25df76dc7d22d0bfddff
Jang Haemin <io@jhaemin.com>
2019-04-23 13:36:20 +0900

    set color scheme on load

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,38 @@
 import './scss/globalstyle'
 
 export default {
+  beforeMount() {
+    this.setColorScheme()
+  },
   mounted() {
     window.addEventListener('dragstart', e => {
       e.preventDefault()
     })
+  },
+  methods: {
+    setColorScheme() {
+      const html = document.documentElement
+
+      html.classList.remove('light-mode')
+      html.classList.remove('dark-mode')
+      html.classList.remove('auto-color-scheme')
+
+      let setting = window.localStorage.getItem('colorScheme')
+      if (setting === 'light') {
+        html.classList.add('light-mode')
+      } else if (setting === 'dark') {
+        html.classList.add('dark-mode')
+      } else if (setting === 'auto') {
+        html.classList.add('auto-color-scheme')
+      } else {
+        window.localStorage.setItem('colorScheme', 'light')
+        this.setColorScheme()
+      }
+    },
+    updateColorScheme(scheme) {
+      window.localStorage.setItem('colorScheme', scheme)
+      this.setColorScheme()
+    }
   },
   name: 'App',
   watch: {
