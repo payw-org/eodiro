9d5d67138a4717d3fc68c3b9dd41a82717f2d5cf
Jang Haemin <io@jhaemin.com>
2019-04-27 22:29:08 +0900

    remove babel for react

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,10 @@ module.exports = {
 					{
 						loader: 'babel-loader',
 						options: {
-							presets: ['@babel/preset-env', '@babel/preset-react']
+							presets: [
+								'@babel/preset-env',
+								// '@babel/preset-react'
+							]
 						}
 					}
 				]
