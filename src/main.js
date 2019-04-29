788c2859867be35377a6279204e622fae8218028
Jang Haemin <io@jhaemin.com>
2019-04-29 16:09:07 +0900

    remove react debris and create Vue instance on window load

--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import Root from './client/Root'
-// import './scss/globalstyle'
-
-// ReactDOM.render(<Root />, document.querySelector('#app'))
-
-
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App'
 import router from './router'
 
-new Vue({
-  el: "#app",
-  router: router,
-  template: '<App />',
-  components: { App }
-})
+window.onload = function () {
+  new Vue({
+    el: "#app",
+    router: router,
+    template: '<App />',
+    components: { App }
+  })
+}
