8b38d85a81af5d9c0271b7fd0e04b4de3d94a55d
Jang Haemin <io@jhaemin.com>
2019-04-29 16:09:23 +0900

    remove active state's styles

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -10,13 +10,17 @@
     >
       <div class="an-container">
         <div class="dummy"></div>
-        <router-link :to="backLink">
-          <h1 class="title">
-            <transition name="fade">
-              <span class="text" :key="mutateNavTitle">{{ mutateNavTitle }}</span>
-            </transition>
-          </h1>
-        </router-link>
+          <div class="single-button-wrapper">
+            <div class="single-button">
+              <router-link :to="backLink">
+                <h1 class="title">
+                  <transition name="fade">
+                    <span class="text" :key="mutateNavTitle">{{ mutateNavTitle }}</span>
+                  </transition>
+                </h1>
+              </router-link>
+            </div>
+          </div>
         <div class="dummy"></div>
       </div>
     </nav>
@@ -110,9 +114,6 @@ export default {
   justify-content: center;
 
   $time: 800ms;
-  transform: translateY(0);
-  opacity: 1;
-  transition: all $time $eodiro-cb;
 
   &.trans-enter-active, &.trans-leave-active {
     transition: all $time $eodiro-cb;
@@ -120,83 +121,77 @@ export default {
   }
   &.trans-enter, &.trans-leave-to,
   &.hidden {
-    transform: translateY(calc(-100% - 2rem));
+    .single-button-wrapper {
+      transform: translateY(calc(-100% - 2rem)) !important;
+      opacity: 0;
+    }
   }
 
   .an-container {
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding-top: 4rem;
     width: calc(100% - 3rem);
     max-width: 80rem;
     margin: auto;
 
-    @include smaller-than($mobile-width-threshold) {
-      padding-top: 3rem;
-    }
-
-    .title {
-      cursor: pointer;
-      position: relative;
-      height: 3rem;
-      text-align: center;
-      white-space: nowrap;
-      font-family: $font-display;
-      font-weight: 700;
-      font-size: 1.3rem;
-      border-radius: 50px;
-      color: #fff;
-      padding: 0 1.4rem;
-      text-align: center;
-      margin: 0 1rem;
-      transition: all 700ms $eodiro-cb;
-      background-color: $light-blue;
-      box-shadow: 0 0.5rem 1.5rem rgba($light-blue, 0.3);
-      overflow: hidden;
-      display: flex;
-      align-items: center;
-      justify-content: center;
+    .single-button-wrapper {
+      padding-top: 4rem;
+      transform: translateY(0);
+      opacity: 1;
+      transition: all $time $eodiro-cb;
+      will-change: transform, opacity;
 
       @include smaller-than($mobile-width-threshold) {
-        font-size: 1rem;
-        height: 2.3rem;
+        padding-top: 3rem;
       }
 
-      .text {
-        position: absolute;
-      }
-
-      &:active {
-        // transition: none;
-        // background-color: darken($light-blue, 15%);
-      }
-
-      &.hover {
-        background-color: $light-green;
-        box-shadow: 0 0.5rem 1.5rem rgba($light-green, 0.3);
-        border-radius: 50px 0.7rem 0.7rem 50px;
-
-        &:active {
-          // background-color: darken($light-green, 15%);
-        }
-      }
-
-      @include dark-mode() {
-        background-color: $light-yellow;
-        box-shadow: 0 0.5rem 1.5rem rgba(#000, 0.3);
-
-        &:active {
-          // transition: none;
-          // background-color: lighten($light-yellow, 15%);
-        }
-
-        &.hover {
-          background-color: $light-red;
-          box-shadow: 0 0.5rem 1.5rem rgba(#000, 0.3);
-
-          &:active {
-            // background-color: lighten($light-red, 15%);
+      .single-button {
+        .title {
+          cursor: pointer;
+          position: relative;
+          height: 3rem;
+          text-align: center;
+          white-space: nowrap;
+          font-family: $font-display;
+          font-weight: 700;
+          font-size: 1.3rem;
+          border-radius: 50px;
+          color: #fff;
+          padding: 0 1.4rem;
+          text-align: center;
+          margin: 0;
+          transition: all 700ms $eodiro-cb;
+          background-color: $light-blue;
+          box-shadow: 0 0.5rem 1.5rem rgba($light-blue, 0.3);
+          overflow: hidden;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        
+          @include smaller-than($mobile-width-threshold) {
+            font-size: 1rem;
+            height: 2.3rem;
+          }
+        
+          .text {
+            position: absolute;
+          }
+        
+          &.hover {
+            background-color: $light-green;
+            box-shadow: 0 0.5rem 1.5rem rgba($light-green, 0.3);
+            border-radius: 50px 0.7rem 0.7rem 50px;
+          }
+        
+          @include dark-mode() {
+            background-color: $light-yellow;
+            box-shadow: 0 0.5rem 1.5rem rgba(#000, 0.3);
+        
+            &.hover {
+              background-color: $light-red;
+              box-shadow: 0 0.5rem 1.5rem rgba(#000, 0.3);
+            }
           }
         }
       }
