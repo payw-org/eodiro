5584260ef3521fda251509035dc60ec337cc9764
Jang Haemin <io@jhaemin.com>
2019-04-30 16:20:42 +0900

    change entry point

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
 	entry: {
-		'eodiro.app': ['./src/main.js']
+		'eodiro.app': ['./client/main.js']
 	},
 	mode: 'development',
 	devtool: 'inline-source-map',
