baca5c9a576551adc2c498c118ea1da07237cb6f
Jang Haemin <io@jhaemin.com>
2019-05-04 13:07:10 +0900

    reconstruct the dom structures

--- client/components/Result.vue
+++ client/components/Result.vue
@@ -2,13 +2,17 @@
   <div class="content-item result">
     <div class="empty-classrooms-container">
       <div
-        class="ec-item"
-        :class="'remaining-time--' + random()"
+        class="ec-item-wrapper"
         v-for="(room, i) in emptyRooms"
         :key="i"
       >
-        <h1 class="room-number">{{ room.roomID }}</h1>
-        <p class="info">다음 수업()까지<br><span class="time">1시간 23분</span> 남았어요.</p>
+        <div
+          class="ec-item"
+          :class="'remaining-time--' + random()"
+        >
+          <h1 class="room-number">{{ room.roomID }}</h1>
+          <p class="info">다음 수업<br><b>[휴먼입니까? ICT - 송노스]</b> 까지<br><span class="time">150시간 2300분</span> 남았어요.</p>
+        </div>
       </div>
     </div>
   </div>
@@ -17,7 +21,7 @@
 <script>
 export default {
   activated() {
-    let rooms = this.$el.querySelectorAll('.ec-item')
+    let rooms = this.$el.querySelectorAll('.ec-item-wrapper')
     rooms.forEach(room => {
       room.classList.remove('appear')
     })
@@ -99,22 +103,9 @@ export default {
       width: calc(100% - 2rem);
     }
 
-    .ec-item {
-      cursor: pointer;
-      padding: 1rem;
-      box-shadow: $eodiro-shadow;
-      border-radius: 1rem;
-      color: $base-white;
+    .ec-item-wrapper {
       opacity: 0;
       transform: translateY(10rem);
-
-      @include dark-mode() {
-        box-shadow: $eodiro-shadow, $dark-mode-border-shadow;
-
-        @include smaller-than($mobile-width-threshold) {
-          box-shadow: $dark-mode-border-shadow;
-        }
-      }
 
       &.appear {
         opacity: 1;
@@ -128,35 +119,59 @@ export default {
         }
       }
 
-      .room-number {
-        font-family: $font-display;
-        font-size: 2.5rem;
-        font-weight: 700;
-        text-align: right;
-        line-height: 1;
-      }
-
-      .info {
-        font-size: 1rem;
-        margin-top: 1rem;
-        line-height: 1.2;
-
-        .time {
-          font-weight: 700;
+      .ec-item {
+        cursor: pointer;
+        padding: 1rem;
+        box-shadow: $eodiro-shadow;
+        border-radius: 1rem;
+        color: $base-white;
+      
+        @include dark-mode() {
+          box-shadow: $eodiro-shadow, $dark-mode-border-shadow;
+      
+          @include smaller-than($mobile-width-threshold) {
+            box-shadow: $dark-mode-border-shadow;
+          }
         }
-      }
-
-      &.remaining-time--1 {
-        background-color: #4e99fc;
-      }
-      &.remaining-time--2 {
-        background-color: #18d687;
-      }
-      &.remaining-time--3 {
-        background-color: #ff9f32;
-      }
-      &.remaining-time--4 {
-        background-color: #f73434;
+      
+        .room-number {
+          font-family: $font-display;
+          font-size: 2.5rem;
+          font-weight: 700;
+          text-align: right;
+          line-height: 1;
+        }
+      
+        .info {
+          font-size: 1rem;
+          margin-top: 2rem;
+          line-height: 1.4;
+          // background-color: rgba(#000, 0.1);
+          border-radius: 0.5rem;
+          // padding: 1rem;
+      
+          .time {
+            font-weight: 700;
+          }
+        }
+      
+        &.remaining-time--1 {
+          background-color: #4e99fc;
+          background-color: #006FFE;
+          background-color: #18B8F9;
+        }
+        &.remaining-time--2 {
+          background-color: #18d687;
+          background-color: #42D359;
+        }
+        &.remaining-time--3 {
+          background-color: #ff9f32;
+          background-color: #FF8902;
+        }
+        &.remaining-time--4 {
+          background-color: #f73434;
+          background-color: #FF264B;
+        }
       }
     }
   }
