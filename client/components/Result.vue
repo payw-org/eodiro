7bdb9c9305bbb0473c4186115d15293b5e7bc89e
Jang Haemin <io@jhaemin.com>
2019-05-05 15:45:59 +0900

    new timetable view

--- client/components/Result.vue
+++ client/components/Result.vue
@@ -8,40 +8,69 @@
       >
         <div
           class="ec-item"
-          :class="'remaining-time--' + random()"
+          :class="'remaining-time--' + (i % 4 + 1)"
+          @click="loadTimeTable(room)"
         >
           <h1 class="room-number">{{ room.roomID }}</h1>
           <p class="info">다음 수업<br><b>[휴먼입니까? ICT - 송노스]</b> 까지<br><span class="time">150시간 2300분</span> 남았어요.</p>
         </div>
       </div>
     </div>
+
+    <transition name="zoom">
+      <div class="timetable-container" v-show="timeTableShow">
+        <div class="background" @click="closeTimeTable()"></div>
+        <div class="timetable">
+          <h1 class="title">{{ selectedRoom }}호 강의 시간표</h1>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+          <p>hello world</p>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import Content from 'Components/Content.vue'
+import SimpleBar from 'simplebar'
+// import 'simplebar/dist/simplebar.css'
+import 'SCSS/simplebar-custom.scss'
+
 export default {
-  activated() {
-    let rooms = this.$el.querySelectorAll('.ec-item-wrapper')
-    rooms.forEach(room => {
-      room.classList.remove('appear')
-    })
-    setTimeout(() => {
-      rooms.forEach(room => {
-        room.classList.add('appear')
-      })
-    }, 4)
-    
-    if (this.isRightDirection) {
-      window.scrollTo(0, 0)
-    } else {
-      window.scrollTo(0, this.scrollPos)
-    }
-  },
-  methods: {
-    random() {
-      return Math.floor(Math.random() * 4) + 1
-    }
-  },
+  name: 'result',
+  extends: Content,
   data() {
     return {
       emptyRooms: [
@@ -77,8 +106,56 @@ export default {
           roomID: 712,
           timeTable: []
         }
-      ]
+      ],
+      timeTableShow: false,
+      selectedRoom: undefined,
+      sbTimeTable: undefined
     }
+  },
+  methods: {
+    random() {
+      return Math.floor(Math.random() * 4) + 1
+    },
+    closeTimeTable() {
+      this.timeTableShow = false
+    },
+    loadTimeTable(room) {
+      this.timeTableShow = true
+      this.selectedRoom = room.roomID
+  
+      // recalculate and reset simplebar
+      setTimeout(() => {
+        this.sbTimeTable.getScrollElement().scrollTo(0,0)
+      }, 0)
+  
+      const interval = window.setInterval(() => {
+        this.sbTimeTable.recalculate()
+      }, 100)
+      window.setTimeout(() => {
+        window.clearInterval(interval)
+      }, 300)
+    },
+    buildIn() {
+      let rooms = this.$el.querySelectorAll('.ec-item-wrapper')
+      rooms.forEach(room => {
+        room.classList.remove('appear')
+      })
+      let i = 0
+      let interval = window.setInterval(() => {
+        rooms[i++].classList.add('appear')
+        if (i === rooms.length) {
+          window.clearInterval(interval)
+        }
+      }, 50)
+    }
+  },
+  mounted() {
+    this.sbTimeTable = new SimpleBar(this.$el.querySelector('.timetable'), {})
+    ;['touchstart', 'mouseover'].forEach(eventName => {
+      this.sbTimeTable.getScrollElement().addEventListener(eventName, e => {
+        this.sbTimeTable.recalculate()
+      })
+    })
   }
 }
 </script>
@@ -111,12 +188,6 @@ export default {
         opacity: 1;
         transform: translateY(0);
         transition: transform 1000ms $eodiro-cb, opacity 1000ms $eodiro-cb;
-      }
-
-      @for $i from 0 through 50 {
-        &:nth-child(#{$i}) {
-          transition-delay: unquote(($i/15) + 's');
-        }
       }
 
       .ec-item {
@@ -172,6 +243,73 @@ export default {
           background-color: #f73434;
           background-color: #FF264B;
         }
+      }
+    }
+  }
+
+  .timetable-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 10000;
+
+    &.zoom-enter-active, &.zoom-leave-active {
+      transition: opacity 0.3s ease;
+      opacity: 1;
+
+      .timetable {
+        transition: opacity 0.3s ease, transform 0.3s ease, filter 0.3s ease;
+        transform: scale(1);
+        opacity: 1;
+        filter: blur(0px);
+      }
+    }
+    &.zoom-enter, &.zoom-leave-to {
+      opacity: 0;
+      
+      .timetable {
+        opacity: 0;
+        transform: scale(0.8);
+        filter: blur(20px);
+      }
+    }
+
+    .background {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+      background-color: rgba(0,0,0,0.4);
+      backdrop-filter: blur(20px);
+    }
+
+    .timetable {
+      padding: 0 1rem;
+      width: calc(100% - 2rem);
+      height: calc(100% - 2rem);
+      max-width: 22rem;
+      max-height: 30rem;
+      background-color: $base-white;
+      border-radius: 1rem;
+      overflow-x: hidden;
+      overflow-y: auto;
+
+      @include dark-mode() {
+        background-color: #333;
+        box-shadow: $dark-mode-border-shadow;
+      }
+
+      .title {
+        font-size: 2rem;
+        font-weight: 700;
+        margin-top: 1.5rem;
       }
     }
   }
