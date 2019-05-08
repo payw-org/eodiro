98f436ce6f6f2b227756e97adf5df036052c3f82
ihooni <1000jaman@naver.com>
2019-05-08 13:01:34 +0900

    Use bodyparser in api app

--- server/app/Provider/APIAppServiceProvider.js
+++ server/app/Provider/APIAppServiceProvider.js
@@ -1,8 +1,11 @@
+import bodyParser from 'body-parser';
 import AppServiceProvider from './AppServiceProvider';
-import { router } from 'Server/app/routes/api'
+import { router } from 'Routes/api'
 
 export default class APIAppServiceProvider extends AppServiceProvider {
     setApp() {
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(bodyParser.json());
         this.app.use(router);
     }
 }
