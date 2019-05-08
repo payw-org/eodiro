219939c5e75acba35e11ca0e236e61eed987b562
ihooni <1000jaman@naver.com>
2019-05-09 02:59:55 +0900

    Rename variables

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import vhost from 'vhost';
-import { api_app_provider, web_app_provider } from './bootstrap';
+import { api_app, web_app } from './bootstrap';
 import { server_config } from './config';
 
 const BASE_URI = "eodiro.com";
@@ -9,7 +9,7 @@ const BASE_URI = "eodiro.com";
 const app = express();
 
 app.use(cors());
-app.use(vhost("api." + BASE_URI, api_app_provider.getApp()));
-app.use(web_app_provider.getApp());
+app.use(vhost("api." + BASE_URI, api_app.getApp()));
+app.use(web_app.getApp());
 
 app.listen(server_config['node_port']);
