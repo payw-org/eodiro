21df77868973c29360cd0928dec241edcd79df2d
Jang Haemin <io@jhaemin.com>
2019-05-10 22:16:37 +0900

    Set default language to Chinese Add title change

--- client/main.js
+++ client/main.js
@@ -9,11 +9,25 @@ let userLang = navigator.language || navigator.userLanguage
 userLang = userLang.slice(0, 2)
 
 // for debuggin
-userLang = 'fr'
+userLang = 'zh'
 
 const i18n = new VueI18n({
   locale: userLang,
   fallbackLocale: 'en'
+})
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta || !to.meta.title) {
+    console.warn('Router doesn\'t have a title')
+  } else {
+    let title = to.meta.title[userLang]
+    if (!title) {
+      title = to.meta.title['en']
+    }
+    document.title = title
+  }
+  
+  next()
 })
 
 new Vue({
