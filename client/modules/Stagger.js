be0089593098d41829c1f50d72e83b8935dc08fc
Jang Haemin <io@jhaemin.com>
2019-05-11 15:08:21 +0900

    Remove driftless

--- client/modules/Stagger.js
+++ client/modules/Stagger.js
@@ -1,20 +1,41 @@
-import { setDriftlessInterval, clearDriftless } from 'driftless'
+import { TweenMax, Power3 } from 'gsap/TweenMax'
 
 export default class Stagger {
   constructor() {}
 
   static animate(elms) {
-    elms.forEach((elm, index) => {
-      elm.classList.remove('appear')
-      elm.getBoundingClientRect().width
-    })
+    for (let i = 0; i < elms.length; i++) {
+      elms[i].classList.remove('appear')
+      // elms[i].style.transitionDelay = i/25 + 's'
+    }
+
+    // let i = 0
+    // let interval = setInterval(() => {
+    //   TweenMax.to(elms[i++], 0.6, {
+    //     ease: Power3.ease,
+    //     opacity: 1,
+    //     transform: 'translateY(0)'
+    //   })
+    //   if (i === elms.length) {
+    //     clearInterval(interval)
+    //   }
+    // }, 30)
+
+    // setTimeout(() => {
+    //   for (let i = 0; i < elms.length; i++) {
+    //     elms[i].classList.add('appear')
+    //     setTimeout(() => {
+    //       elm[i].style.transitionDelay = ''
+    //     }, i/25 * 1000)
+    //   }
+    // }, 100)
 
     let i = 0
-    let interval = setDriftlessInterval(() => {
+    let interval = setInterval(() => {
       elms[i++].classList.add('appear')
       if (i === elms.length) {
-        clearDriftless(interval)
+        clearInterval(interval)
       }
-    }, 30)
+    }, 40)
   }
 }
