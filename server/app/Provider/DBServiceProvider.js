16861f008b260426eb6306bde192e17f0bca817b
ihooni <1000jaman@naver.com>
2019-05-14 15:26:15 +0900

    Resolve merge conflict

--- server/app/Provider/DBServiceProvider.js
+++ server/app/Provider/DBServiceProvider.js
@@ -1,15 +1,15 @@
 import DBConnector from 'DB/DBConnector';
-// import DBInitializer from 'DB/DBInitializer';
+import DBInitializer from 'DB/DBInitializer';
 
 export default class DBServiceProvider {
   constructor() {
     this.dbConnector = new DBConnector();
-    // this.dbInitializer = new DBInitializer();
+    this.dbInitializer = new DBInitializer();
   }
 
   async boot() {
     await this.dbConnector.connect();
-    await this.dbInitializer.initialize();
+    await this.dbInitializer.initialize('drop');
 
     return Promise.resolve();
   }
