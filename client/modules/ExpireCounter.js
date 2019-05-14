4494a90d731d7a1d7b16e96678789f6ac203df3b
Jang Haemin <io@jhaemin.com>
2019-05-14 21:55:00 +0900

    Upgrade the timetable counter module

--- client/modules/ExpireCounter.js
+++ client/modules/ExpireCounter.js
@@ -1,81 +1,104 @@
 
-export default class ExpireCounter{
-    constructor(classOfFloar){
-        this.classOfFloar = classOfFloar;
+export default class ExpireCounter {
+  constructor(classOfFloor) {
+    this.classOfFloor = classOfFloor;
+  }
+
+  /**
+   * return room state about roomId atDate
+   * roomState.nextClassName = String
+   * roomState.expireTime = number (minutes)
+   * roomState.expireTimeLevel = nubmer (level per 30 minutes)
+   * @param {number} roomId 
+   * @param {Date} atDate 
+   */
+  run(roomId, atDate) {
+    var roomState = new Object;
+    var classOfRoom;
+    var classOfRoomOnDay = new Array;
+    var atDateByValue;
+
+
+    // get class array in room
+    this.classOfFloor.forEach(function (item, index) {
+      if (item['number'] == roomId)
+        classOfRoom = item['lectures'];
+    });
+
+    // get class array on the day
+    var day = this.parseDayNumberToDay(atDate.getDay());
+    classOfRoom.forEach(function (item, index) {
+      if (item['time']['day'] == day) {
+        classOfRoomOnDay.push(item);
+      }
+    });
+
+    // sort classOfRoom
+    classOfRoomOnDay.sort((classA, classB) => {
+      var valueOfA = this.parseClassTimeToValue(classA['time']['start']);
+      var valueOfB = this.parseClassTimeToValue(classB['time']['start']);
+      return valueOfA - valueOfB;
+    });
+
+    // make atTime hour-minute-second to second
+    atDateByValue = atDate.getHours() * 3600 + atDate.getMinutes() * 60 + atDate.getSeconds();
+
+    // get next class and assign roomState property
+    for (var i = 0; i < classOfRoomOnDay.length; i++) {
+      let item = classOfRoomOnDay[i];
+
+      if (atDateByValue < this.parseClassTimeToValue(item['time']['end'])) {
+        // class is up coming
+        if (atDateByValue < this.parseClassTimeToValue(item['time']['start'])) {
+          roomState.nextClassName = item['name'];
+          roomState.expireTime = (this.parseClassTimeToValue(item['time']['start']) - atDateByValue) / 60;
+          roomState.expireTimeLevel = parseInt(roomState.expireTime / 30);
+          break;
+        }
+        // class is now
+        else {
+          roomState.nextClassName = item['name'];
+          roomState.expireTime = (this.parseClassTimeToValue(item['time']['end']) - atDateByValue) / 60;
+          roomState.expireTimeLevel = -1;
+          break;
+        }
+      }
+
     }
 
-    /**
-     * 
-     * @param {number} roomId 
-     * @param {Date} atDate 
-     */
-    run(roomId, atDate){
-        var roomState = new Object;
-        var classOfRoom;
-        var classOfRoomOnDay = new Array;
-        var atDateByValue;
 
-        // get class array in room
-        this.classOfFloar.forEach(function(item,index){
-            if(item['num'] == roomId)
-                classOfRoom = item['lectures'];
-        });
+    return roomState;
+  }
 
-        // get class array on the day
-        var day = this.parseDayNumberToDay(atDate.getDay());
-        classOfRoom.forEach(function(item,index){
-            if(item['time']['day'] == day){
-               classOfRoomOnDay.push(item);
-            }
-        });
+  /**
+   * classTime's form is "##:##"
+   * return time to second
+   * @param {String} classTime 
+   */
+  parseClassTimeToValue(classTime) {
+    return parseInt(classTime.substr(0, 2)) * 3600 + parseInt(classTime.substr(2, 2) * 60);
+  }
 
-        // sort classOfRoom
-        classOfRoomOnDay.sort(function(classA,classB){
-            var valueOfA = parseClassTimeToValue(classA['time']['start']);
-            var valueOfB = parseClassTimeToValue(classB['time']['start']);
-            return valueOfA-valueOfB;
-        });
+  /**
+   * parser number To Day {"Sun","Mon", ...}
+   * @param {number} dayNumber 
+   */
+  parseDayNumberToDay(dayNumber) {
+    if (dayNumber == 0)
+      return "SUN"
+    if (dayNumber == 1)
+      return "MON"
+    if (dayNumber == 2)
+      return "TUE"
+    if (dayNumber == 3)
+      return "WED"
+    if (dayNumber == 4)
+      return "THU"
+    if (dayNumber == 5)
+      return "FRI"
+    if (dayNumber == 6)
+      return "SAT"
 
-        // make atTime hour-minute-second to second
-        atDateByValue = atDate.getHours()*3600 + atDate.getMinutes()*60 + atDate.getSeconds;
- 
-        // get next class and assign roomState property
-        classOfRoomOnDay.forEach(function(item,index){
-            if( atDateByValue < parseClassTimeToValue(item['time']['start'])){
-                roomState.nextClassName = item['name'];
-                roomState.expireTime = (parseClassTimeToValue(item['time']['start']) - atDateByValue)/60;
-                roomState.expireTimeLevel = roomState.expireTime%1800;
-            }
-        });
-
-        return roomState;
-    }
-
-    /**
-     * classTime's form is "##:##"
-     * return time to second
-     * @param {String} classTime 
-     */
-    parseClassTimeToValue(classTime){
-        return parseInt(classTime.substr(0,2))*3600 + parseInt(classTime.substr(3,2)*60);
-    }
-
-    parseDayNumberToDay(dayNumber){
-        if(dayNumber == 0)
-            return "Sun"
-        if(dayNumber == 1)
-            return "Mon"
-        if(dayNumber == 2)
-            return "Tue"
-        if(dayNumber == 3)
-            return "Wed"
-        if(dayNumber == 4)
-            return "Thu"
-        if(dayNumber == 5)
-            return "Fri"
-        if(dayNumber == 6)
-            return "Sat"
-        
-        return "Error-Day"
-    }
+    return "Error-Day"
+  }
 }
