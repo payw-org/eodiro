9ef7d067a09866d6752f19eca1fb38c2b68e99d4
Jang Haemin <io@jhaemin.com>
2019-05-20 17:29:17 +0900

    More stable staggering animation

--- client/modules/Stagger.js
+++ client/modules/Stagger.js
@@ -2,7 +2,17 @@ export default class Stagger {
   constructor() {}
 
   /**
-   * 
+   * Returns a callback function to be used in setTimeout
+   * @param {HTMLElement} elm 
+   */
+  static showElement(elm) {
+    return function () {
+      elm.classList.add('stagger-appear')
+    }
+  }
+
+  /**
+   * Show elements by adding classnames sequentially
    * @param {Array<HTMLElement>} elms 
    */
   static show(elms, hasParent = false) {
@@ -10,6 +20,12 @@ export default class Stagger {
       return
     }
 
+    if (elms[0].classList.contains('stagger-appear') || elms[0].classList.contains('stagger-appear-fix')) {
+      return
+    }
+
+    // add fixed class name ('stagger-appear-fix' -> opacity: 0)
+    // to the elements which are not in the viewport
     let i = 0
     for (; i < elms.length; i++) {
       let boundaryTarget = elms[i]
@@ -30,23 +46,43 @@ export default class Stagger {
       }
     }
 
-    let interval = window.setInterval(() => {
-      if (!elms[i]) {
-        window.clearInterval(interval)
-        return
-      } else {
-        if (elms[i].classList.contains('stagger-appear') || elms[i].classList.contains('stagger-appear-fix')) {
-          window.clearInterval(interval)
-          return
-        } else {
-          elms[i++].classList.add('stagger-appear')
-        }
+    // @deprecated
+    // using forEach
+
+    // window.setTimeout(() => {
+    //   elms.forEach((elm, i) => {
+    //     window.setTimeout(this.showElement(elm), i * 50)
+    //   })
+    // }, 0)
+
+    for (let j = 0; i < elms.length; i++, j++) {
+      if (elms[i].classList.contains('stagger-appear') || elms[i].classList.contains('stagger-appear-fix')) {
+        break
       }
-    }, 50)
+      let callback = this.showElement(elms[i])
+      setTimeout(callback, j * 55);
+    }
+
+    // @deprecated
+    // using setInterval
+
+    // let interval = window.setInterval(() => {
+    //   if (!elms[i]) {
+    //     window.clearInterval(interval)
+    //     return
+    //   } else {
+    //     if (elms[i].classList.contains('stagger-appear') || elms[i].classList.contains('stagger-appear-fix')) {
+    //       window.clearInterval(interval)
+    //       return
+    //     } else {
+    //       elms[i++].classList.add('stagger-appear')
+    //     }
+    //   }
+    // }, 50)
   }
 
   /**
-   * 
+   * Hide elements by removing classnames
    * @param {Array<HTMLElement>} elms 
    */
   static hide(elms) {
