27d37d3e5f9193872bdc001be8b0158832607e55
Jang Haemin <io@jhaemin.com>
2019-05-22 00:18:12 +0900

    New preparing page

---
+++ client/components/Preparing.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="preparing">
+    <h1>업데이트중입니다.<br>잠시만 기다려주세요.</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import 'SCSS/global-variables.scss';
+
+#preparing {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  h1 {
+    text-align: center;
+    max-width: calc(100% - 2rem);
+  }
+}
+</style>
