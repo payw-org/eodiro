8b925430fc83a2a3bf29bf9af89b5829b4af21b4
Jang Haemin <io@jhaemin.com>
2019-07-05 10:05:19 +0900

    Reformat with prettier

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -53,67 +53,95 @@
 
 <template>
   <div id="home">
-    <p class="about-developers">{{ thisYear }} © eodiro |&nbsp;<a href="mailto:support@eodiro.com" class="contact">{{ $t('contact') }}</a></p>
+    <p class="about-developers">
+      {{ thisYear }} © eodiro |&nbsp;
+      <a href="mailto:support@eodiro.com" class="contact">{{ $t('contact') }}</a>
+    </p>
     <!-- <transition
       appear
       name="zoom"
-    > -->
-      <div class="start-box">
-        <div class="content-area">
-          <div class="main" v-show="!isSettingsActive" key="1">
-            <div class="logo-container">
-              <svg width="122px" height="198px" viewBox="0 0 122 198" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                <!-- Generator: Sketch 54.1 (76490) - https://sketchapp.com -->
-                <title>Fill 3</title>
-                <desc>Created with Sketch.</desc>
-                <defs>
-                  <linearGradient x1="52.8981037%" y1="0%" x2="38.2508483%" y2="101.650823%" id="linearGradient-1">
-                    <stop stop-color="#FFDF00" offset="0%"></stop>
-                    <stop stop-color="#FF006D" offset="100%"></stop>
-                  </linearGradient>
-                </defs>
-                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                  <g id="eodiro_logo_r" transform="translate(-102.000000, -52.000000)" fill="url(#linearGradient-1)">
-                    <g id="eodiro_logo" transform="translate(102.000000, 52.000000)">
-                      <path d="M66.059,33.499 L57.209,24.649 C55.111,22.551 53.933,19.705 53.933,16.738 C53.933,13.771 55.111,10.926 57.209,8.828 C58.796,7.241 60.464,5.573 62.051,3.986 C64.149,1.888 66.994,0.71 69.962,0.71 C72.929,0.71 75.774,1.888 77.872,3.986 C88.646,14.76 107.822,33.936 118.596,44.71 C122.965,49.079 122.965,56.162 118.596,60.531 C107.822,71.305 88.646,90.481 77.872,101.255 C75.774,103.353 72.929,104.532 69.962,104.532 C66.994,104.532 64.149,103.353 62.051,101.255 C60.464,99.668 58.796,98.001 57.209,96.414 C55.111,94.316 53.933,91.47 53.933,88.503 C53.933,85.536 55.111,82.69 57.209,80.592 L66.007,71.794 L49.985,71.794 C43.807,71.794 38.798,76.803 38.798,82.982 C38.798,106.787 38.798,162.225 38.798,186.031 C38.798,192.209 33.789,197.218 27.611,197.218 C22.599,197.218 16.702,197.218 11.69,197.218 C8.722,197.218 5.877,196.039 3.779,193.941 C1.681,191.843 0.502,188.998 0.502,186.031 C0.502,161.446 0.502,101.962 0.502,64.963 C0.502,56.618 3.817,48.615 9.718,42.714 C15.619,36.813 23.622,33.499 31.966,33.499 L66.059,33.499 Z" id="Fill-3"></path>
-                    </g>
+    >-->
+    <div class="start-box">
+      <div class="content-area">
+        <div class="main" v-show="!isSettingsActive" key="1">
+          <div class="logo-container">
+            <svg
+              width="122px"
+              height="198px"
+              viewBox="0 0 122 198"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+            >
+              <!-- Generator: Sketch 54.1 (76490) - https://sketchapp.com -->
+              <title>Fill 3</title>
+              <desc>Created with Sketch.</desc>
+              <defs>
+                <linearGradient
+                  x1="52.8981037%"
+                  y1="0%"
+                  x2="38.2508483%"
+                  y2="101.650823%"
+                  id="linearGradient-1"
+                >
+                  <stop stop-color="#FFDF00" offset="0%" />
+                  <stop stop-color="#FF006D" offset="100%" />
+                </linearGradient>
+              </defs>
+              <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g
+                  id="eodiro_logo_r"
+                  transform="translate(-102.000000, -52.000000)"
+                  fill="url(#linearGradient-1)"
+                >
+                  <g id="eodiro_logo" transform="translate(102.000000, 52.000000)">
+                    <path
+                      d="M66.059,33.499 L57.209,24.649 C55.111,22.551 53.933,19.705 53.933,16.738 C53.933,13.771 55.111,10.926 57.209,8.828 C58.796,7.241 60.464,5.573 62.051,3.986 C64.149,1.888 66.994,0.71 69.962,0.71 C72.929,0.71 75.774,1.888 77.872,3.986 C88.646,14.76 107.822,33.936 118.596,44.71 C122.965,49.079 122.965,56.162 118.596,60.531 C107.822,71.305 88.646,90.481 77.872,101.255 C75.774,103.353 72.929,104.532 69.962,104.532 C66.994,104.532 64.149,103.353 62.051,101.255 C60.464,99.668 58.796,98.001 57.209,96.414 C55.111,94.316 53.933,91.47 53.933,88.503 C53.933,85.536 55.111,82.69 57.209,80.592 L66.007,71.794 L49.985,71.794 C43.807,71.794 38.798,76.803 38.798,82.982 C38.798,106.787 38.798,162.225 38.798,186.031 C38.798,192.209 33.789,197.218 27.611,197.218 C22.599,197.218 16.702,197.218 11.69,197.218 C8.722,197.218 5.877,196.039 3.779,193.941 C1.681,191.843 0.502,188.998 0.502,186.031 C0.502,161.446 0.502,101.962 0.502,64.963 C0.502,56.618 3.817,48.615 9.718,42.714 C15.619,36.813 23.622,33.499 31.966,33.499 L66.059,33.499 Z"
+                      id="Fill-3"
+                    />
                   </g>
                 </g>
-              </svg>
-            </div>
-            <h1 class="logo-text">{{ $t('title') }}</h1>
-            <p class="description base-gray">{{ $t('description') }}</p>
-            <router-link :to="'/' + startLink"><button class="go-btn eodiro-btn">{{ startMsg }} <span class="arrow">→</span></button></router-link>
+              </g>
+            </svg>
           </div>
-          <div class="color-scheme-config" v-show="isSettingsActive" key="2">
-            <button
-              class="mode-btn light"
-              @click="$emit('changeColorScheme', 'light')"
-            >{{ $t('light_mode') }}</button>
-            <button
-              class="mode-btn dark"
-              @click="$emit('changeColorScheme', 'dark')"
-            >{{ $t('dark_mode') }}</button>
-            <button
-              class="mode-btn auto"
-              v-if="autoDarkModeSupport"
-              @click="$emit('changeColorScheme', 'auto')"
-            >{{ $t('adaptive') }} (macOS Mojave)</button>
-          </div>
+          <h1 class="logo-text">{{ $t('title') }}</h1>
+          <p class="description base-gray">{{ $t('description') }}</p>
+          <nuxt-link :to="'/' + startLink">
+            <button class="go-btn eodiro-btn">
+              {{ startMsg }}
+              <span class="arrow">→</span>
+            </button>
+          </nuxt-link>
         </div>
-        <div class="settings-area">
+        <div class="color-scheme-config" v-show="isSettingsActive" key="2">
           <button
-            class="config-btn color-scheme-pref"
-            @click="isSettingsActive = !isSettingsActive"
-          >
-            <span v-if="!isSettingsActive">{{ $t('colorMode') }}</span>
-            <span v-else>{{ $t('done') }}</span>
-          </button>
-          <router-link to="/university">
-          <button class="config-btn" v-if="isDefaultUniversityExist && !isSettingsActive">{{ $t('changeCollege') }}</button>
-          </router-link>
+            class="mode-btn light"
+            @click="$emit('changeColorScheme', 'light')"
+          >{{ $t('light_mode') }}</button>
+          <button
+            class="mode-btn dark"
+            @click="$emit('changeColorScheme', 'dark')"
+          >{{ $t('dark_mode') }}</button>
+          <button
+            class="mode-btn auto"
+            v-if="autoDarkModeSupport"
+            @click="$emit('changeColorScheme', 'auto')"
+          >{{ $t('adaptive') }} (macOS Mojave)</button>
         </div>
       </div>
+      <div class="settings-area">
+        <button class="config-btn color-scheme-pref" @click="isSettingsActive = !isSettingsActive">
+          <span v-if="!isSettingsActive">{{ $t('colorMode') }}</span>
+          <span v-else>{{ $t('done') }}</span>
+        </button>
+        <nuxt-link :to="localePath('university')">
+          <button
+            class="config-btn"
+            v-if="isDefaultUniversityExist && !isSettingsActive"
+          >{{ $t('changeCollege') }}</button>
+        </nuxt-link>
+      </div>
+    </div>
     <!-- </transition> -->
     <!-- <HomeBGTile/> -->
   </div>
@@ -123,7 +151,7 @@
 import HomeBGTile from '~/components/HomeBGTile.vue'
 
 export default {
-  head () {
+  head() {
     return {
       title: this.$t('title'),
       meta: [
@@ -162,7 +190,9 @@ export default {
     startMsg() {
       if (process.client) {
         if (window.localStorage.getItem('defaultUniversity')) {
-          const defaultUniversity = JSON.parse(window.localStorage.getItem('defaultUniversity'))
+          const defaultUniversity = JSON.parse(
+            window.localStorage.getItem('defaultUniversity')
+          )
           this.isDefaultUniversityExist = true
           this.startLink = defaultUniversity.vendor
           return defaultUniversity.name
@@ -194,7 +224,7 @@ export default {
 
 #home {
   $transition-time: 1200ms;
-  $cb: cubic-bezier(.24,.49,.01,.99);
+  $cb: cubic-bezier(0.24, 0.49, 0.01, 0.99);
   $transition-property: $transition-time $cb;
 
   position: fixed;
@@ -265,7 +295,7 @@ export default {
     height: 90%;
     min-height: 23rem;
     max-height: 23rem;
-    box-shadow: 0 1.5rem 5rem rgba(0,0,0,0.15);
+    box-shadow: 0 1.5rem 5rem rgba(0, 0, 0, 0.15);
     text-align: center;
     overflow: hidden;
     will-change: transform;
@@ -278,26 +308,30 @@ export default {
 
     @include dark-mode() {
       background-color: #222;
-      box-shadow: 0 1rem 2rem rgba(0,0,0,0.3), $dark-mode-border-shadow;
+      box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.3), $dark-mode-border-shadow;
     }
 
     @include smaller-than(700px) {
       width: calc(100% - 2rem);
     }
 
-    &.zoom-enter-active, &.zoom-leave-active {
+    &.zoom-enter-active,
+    &.zoom-leave-active {
       transform: scale(1);
       filter: blur(0px);
       opacity: 1;
-      box-shadow: 0 1rem 2rem rgba(0,0,0,0.1);
+      box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.1);
       transition-delay: 0.2;
-      transition: transform $transition-time $cb, opacity $transition-time/2 $cb, filter $transition-time $cb, background-color $transition-property, box-shadow $transition-property;
+      transition: transform $transition-time $cb, opacity $transition-time/2 $cb,
+        filter $transition-time $cb, background-color $transition-property,
+        box-shadow $transition-property;
 
       @include dark-mode() {
-        box-shadow: 0 1rem 2rem rgba(0,0,0,0.3), $dark-mode-border-shadow;
+        box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.3), $dark-mode-border-shadow;
       }
     }
-    &.zoom-enter, &.zoom-leave-to {
+    &.zoom-enter,
+    &.zoom-leave-to {
       transform: scale(0.7);
       filter: blur(30px);
       opacity: 0;
@@ -339,7 +373,8 @@ export default {
           }
         }
 
-        .logo-text, .description {
+        .logo-text,
+        .description {
           transition: color 500ms ease;
         }
 
@@ -358,7 +393,8 @@ export default {
 
         .go-btn {
           margin: 2rem auto 0;
-          transition: background-color $transition-time $cb, box-shadow $transition-time $cb;
+          transition: background-color $transition-time $cb,
+            box-shadow $transition-time $cb;
           display: flex;
 
           .arrow {
@@ -436,14 +472,15 @@ export default {
         background-color: #f4f4f4;
         padding: 0 0.6rem;
         border-radius: 50px;
-        transition: background-color $transition-time $cb, color $transition-time $cb;
+        transition: background-color $transition-time $cb,
+          color $transition-time $cb;
         margin-right: 0.5rem;
         vertical-align: top;
 
         &:last-child {
           margin-right: 0;
         }
-        
+
         @include dark-mode() {
           color: $base-white;
           background-color: #444;
@@ -452,5 +489,4 @@ export default {
     }
   }
 }
-
 </style>
