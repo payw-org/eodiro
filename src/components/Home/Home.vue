7b229eb556113b2aa5b0613a93f12c5dd332b7dc
Jang Haemin <io@jhaemin.com>
2019-07-16 17:00:18 +0900

    Add a scoped attribute to style

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -194,7 +194,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import '~/assets/styles/scss/global-variables.scss';
 @import '~/assets/styles/scss/global-mixins.scss';
 
