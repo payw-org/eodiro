0dd0448301cd4d850601726c6b045a5076f2a039
Jang Haemin <io@jhaemin.com>
2019-07-17 15:08:20 +0900

    Test browser language

--- src/plugins/init.js
+++ src/plugins/init.js
@@ -3,4 +3,11 @@ import ColorScheme from '~/plugins/ColorScheme.ts'
 // set color scheme on load
 if (!process.server) {
   ColorScheme.setColorScheme()
+
+  // testing
+  // detect browser language and recommend right language url
+  let browserLang = navigator.language.split('-')
+  let urlLang = window.location.pathname.split('/')[1]
+
+  console.log(`browser: ${browserLang}, route: ${urlLang}`)
 }
