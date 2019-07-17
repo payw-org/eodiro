597d931ebe872e993af9ee2cce5e94a523887cb4
Jang Haemin <io@jhaemin.com>
2019-07-17 15:07:15 +0900

    Upgrade mouseclick and touchdown actions

--- src/components/home/MenuItem.vue
+++ src/components/home/MenuItem.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="menu-item">
-    <div class="menu-icon-area">
-      <div class="menu-icon-wrapper">
-        <span class="menu-icon"></span>
+  <div
+    class="menu-item"
+    :class="{active: clicked}"
+    @mousedown="clicked = true"
+    @mouseup="clicked = false"
+    @mouseleave="clicked = false"
+    @touchstart="clicked = true"
+    @touchend="clicked = false"
+    @touchmove="clicked = false"
+  >
+    <div class="content">
+      <div class="menu-icon-area">
+        <div class="menu-icon-wrapper">
+          <span class="menu-icon"></span>
+        </div>
       </div>
-    </div>
-    <h1 class="title">
-      <slot name="title"></slot>
-    </h1>
-    <div class="arrow-wrapper">
-      <span class="arrow"></span>
+      <h1 class="title">
+        <slot name="title"></slot>
+      </h1>
+      <div class="arrow-wrapper">
+        <span class="arrow"></span>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      clicked: false
+    }
+  }
+}
+</script>
+
 
 <style lang="scss" scoped>
 @import '~/assets/styles/scss/global-variables.scss';
@@ -20,10 +42,23 @@
 .menu-item {
   display: flex;
   border-radius: 8px;
-  transition: background-color 100ms ease;
+  transition: background-color 1000ms ease, box-shadow 1000ms ease;
 
-  &:active {
-    background-color: $eodiro-gray;
+  .content {
+    display: flex;
+    transition: transform 1000ms ease;
+  }
+
+  &.active {
+    background-color: $gray;
+    background-color: $light-gray;
+    box-shadow: inset 0 0 0.3rem rgba(0, 0, 0, 0.2);
+    transition: background-color 100ms ease, box-shadow 100ms ease;
+
+    .content {
+      transform: scale(0.95);
+      transition: transform 100ms ease;
+    }
   }
 
   &.vacant {
@@ -60,7 +95,7 @@
       display: block;
       width: 2rem;
       height: 2rem;
-      background-image: linear-gradient(to bottom, $edr-step--3, $edr-step--4);
+      background-image: linear-gradient(to bottom, $c-step--3, $c-step--4);
       border-radius: 5px;
 
       .menu-icon {
@@ -79,7 +114,7 @@
     font-weight: 600;
     flex: 1;
     margin: 2rem 0;
-    color: $edr-step--4;
+    color: $c-step--4;
   }
 
   .arrow-wrapper {
