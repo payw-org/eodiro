5ce3c8459dc6410bff0a03710ad49180edacf214
Jang Haemin <io@jhaemin.com>
2019-07-20 22:43:13 +0900

    Remove client side color scheme initializing

--- src/plugins/init.ts
+++ src/plugins/init.ts
@@ -2,14 +2,15 @@ import ColorScheme from '~/plugins/ColorScheme.ts'
 import Cookie from 'cookie'
 
 export default context => {
-  const { app, req, route, store, redirect } = context
+  const { app, req, res, route, store, redirect } = context
 
   if (process.server) {
     if (!route.name) {
       return
     }
     // browser redirection
-    const cookies = Cookie.parse(req.headers.cookie)
+    const cookies =
+      req.headers && req.headers.cookie ? Cookie.parse(req.headers.cookie) : {}
     const redirectLang = cookies['i18n_redirected']
     if (!redirectLang) {
       return
@@ -31,18 +32,5 @@ export default context => {
         redirect(`/en${route.path.replace(/\/$/, '')}`)
       }
     }
-  }
-
-  // only client side
-  if (process.client) {
-    // set color scheme on load
-    ColorScheme.setColorScheme()
-
-    // testing
-    // detect browser language and recommend right language url
-    let browserLang = navigator.language.split('-')
-    let urlLang = window.location.pathname.split('/')[1]
-
-    console.log(`browser: ${browserLang}, route: ${urlLang}`)
   }
 }
