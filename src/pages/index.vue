4213b55bc01e103867ca2ac362435d50e2ad0ade
Jang Haemin <io@jhaemin.com>
2019-07-20 16:00:59 +0900

    Remove legacy Home component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,13 @@
 <template>
-  <div>
-    <new-home />
-  </div>
+  <new-home />
 </template>
 
 <script>
-import Home from '~/components/home/Home'
 import NewHome from '~/components/home/NewHome'
 import Modal from '~/plugins/eodiro-modal'
 
 export default {
-  components: { Home, NewHome },
+  components: { NewHome },
   mounted() {
     const modal = new Modal()
   }
