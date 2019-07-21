bbf54ba0c28b51844796ae1b8adb7678c6b6d13a
Jang Haemin <io@jhaemin.com>
2019-07-21 14:46:05 +0900

    Add title and description

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,3 +1,16 @@
+<i18n>
+{
+  "kr": {
+    "title": "어디로",
+    "description": "중앙대학교 길잡이"
+  },
+  "en": {
+    "title": "eodiro",
+    "description": "CAU Guidance"
+  }
+}
+</i18n>
+
 <template>
   <div class="wrapper">
     <nuxt></nuxt>
@@ -8,6 +21,22 @@
 export default {
   head() {
     return {
+      title: this.$t('title'),
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.$t('description')
+        },
+        {
+          property: 'og:title',
+          content: this.$t('title')
+        },
+        {
+          property: 'og:description',
+          content: this.$t('description')
+        }
+      ],
       htmlAttrs: {
         class: this.$store.state.colorSchemeClassName
       }
