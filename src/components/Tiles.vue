27b6dd354f53da856e565eb8519faafd28009b9e
hchangjae <hcj@payw.org>
2019-07-24 22:03:10 +0900

    tring to make blink tile

--- src/components/Tiles.vue
+++ src/components/Tiles.vue
@@ -1,79 +1,190 @@
 <template>
-  <div class="tiles-container">
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
-    <div class="a-tile"></div>
+  <div id="tiles-container">
+    <div
+      v-for="tileState in tileStates"
+      :class="'tile ' + tileState.className"
+      :key="tileState.key"
+    ></div>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      show: false
+      key: 0,
+      tileStates: undefined,
+      visibleTilesNum: 0,
+      interval: 0,
+      resizeEvent: null
     }
   },
   methods: {
-    showComment() {
-      this.show = true
+    calculateVisibleTilesNumber() {
+      let tiles = this.$el.querySelectorAll('.tile')
+      let width_max = window.innerWidth
+      let height_max = window.innerHeight
+      let tileNum_height, tileNum_width
+
+      let rem = parseInt(
+        window.getComputedStyle(document.body).getPropertyValue('font-size'),
+        10
+      )
+      let width_tile
+
+      if (width_max >= 1400) {
+        rem *= 1.3
+      } else if (width_max >= 700) {
+        rem *= 1.1
+      } else {
+        rem *= 0.85
+      }
+
+      width_max *= 1.1
+      height_max *= 1.25
+      if (width_max >= 700) {
+        // minimize width_tile
+        width_tile = 4 * rem
+
+        // calc tileNum_width in minimized width_tile and round it
+        tileNum_width = Math.floor(
+          (width_max + 1.5 * rem) / (width_tile + 1.5 * rem)
+        )
+        // calc real width_tile
+        width_tile = (width_max + 1.5 * rem) / tileNum_width - 1.5 * rem
+        // calc tileNum_height
+        tileNum_height = Math.floor(
+          ((height_max * 35) / 100 + 1.5 * rem) / (width_tile + 1.5 * rem)
+        )
+      } else {
+        // minimize width_tile
+        width_tile = 3 * rem
+
+        // calc tileNum_width in minimized width_tile and round it
+        tileNum_width = Math.floor(
+          (width_max + 1 * rem) / (width_tile + 1 * rem)
+        )
+        // calc real width_tile
+        width_tile = (width_max + 1 * rem) / tileNum_width - 1 * rem
+        // calc tileNum_height
+        tileNum_height = Math.floor(
+          ((height_max * 35) / 100 + 1 * rem) / (width_tile + 1 * rem)
+        )
+      }
+      console.log(width_tile)
+      console.log(tileNum_width)
+      console.log(tileNum_height)
+      return tileNum_height * tileNum_width
+    },
+    createTiles() {
+      let ts = []
+      let tileNum = this.calculateVisibleTilesNumber()
+      for (let i = 0; i < tileNum; i++) {
+        ts.push({
+          key: i,
+          className: 'color-1'
+        })
+      }
+      this.tileStates = ts
+
+      // Use nextTick to run animation after
+      // the elements are rendered with the data input
+      this.$nextTick(() => {
+        this.activateAnimation()
+      })
+    },
+    activateAnimation() {
+      let i, c
+      let tileNum = this.calculateVisibleTilesNumber()
+      this.interval = setInterval(() => {
+        for (let a = 0; a < Math.ceil(tileNum / 20); a++) {
+          i = Math.floor(Math.random() * tileNum)
+          c = Math.floor(Math.random() * 9)
+          this.tileStates[i]['className'] = 'color-' + c
+        }
+      }, 100)
     }
+  },
+  mounted() {
+    this.createTiles()
+    window.addEventListener(
+      'resize',
+      (this.resizeEvent = () => {
+        window.clearInterval(this.interval)
+        this.activateAnimation()
+      })
+    )
+  },
+  beforeDestroy() {
+    clearInterval(this.interval)
+    window.removeEventListener('resize', this.resizeEvent)
   }
 }
 </script>
 <style lang="scss">
-.tiles-container {
+@import '~/assets/styles/scss/global-variables.scss';
+@import '~/assets/styles/scss/global-mixins.scss';
+#tiles-container {
   position: absolute;
   width: 110%;
   height: 125%;
-  overflow: visible;
+  overflow: hidden;
   display: grid;
-  grid-gap: 5%;
-  // padding: 5% 0;
-  grid-template-columns: repeat(7, 10%);
-
-  .a-tile {
-    background-color: #ff9f8f;
-    // background-color: #ff3852;
+  grid-gap: 1.5rem;
+  grid-template-columns: repeat(auto-fit, minmax(4rem, 1fr));
+  @include smaller-than(700px) {
+    grid-gap: 1rem;
+    grid-template-columns: repeat(auto-fit, minmax(3rem, 1fr));
+  }
+  .tile {
     border-radius: 30%;
-    padding-bottom: 100%;
+    transition: background-color 500ms linear;
+    @include smaller-than(700px) {
+      border-radius: 0.8rem;
+    }
+    &::before {
+      content: '';
+      display: block;
+      padding-top: 100%;
+    }
+    &.color-1 {
+      background-color: transparent;
+    }
+    &.color-2 {
+      background-color: transparent;
+    }
+    &.color-3 {
+      background-color: transparent;
+    }
+    &.color-4 {
+      background-color: #fcfcff;
+      @include dark-mode() {
+        background-color: #363636;
+      }
+    }
+    &.color-5 {
+      background-color: #fafafd;
+      @include dark-mode() {
+        background-color: #383838;
+      }
+    }
+    &.color-6 {
+      background-color: #f4f4f7;
+      @include dark-mode() {
+        background-color: #414141;
+      }
+    }
+    &.color-7 {
+      background-color: #f8f8fb;
+      @include dark-mode() {
+        background-color: #4b4b4b;
+      }
+    }
+    &.color-8 {
+      background-color: #e4e4e7;
+      @include dark-mode() {
+        background-color: #686868;
+      }
+    }
   }
 }
 </style>
