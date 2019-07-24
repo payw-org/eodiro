7a26debcdf466911b96105a65a2f594f3e40cfcb
Jang Haemin <io@jhaemin.com>
2019-07-24 17:30:45 +0900

    Push history when each page loads

--- src/components/EodiroBase.vue
+++ src/components/EodiroBase.vue
@@ -1,15 +1,24 @@
 <script>
 // eodiro global base component
-// always extends this component when you create a page
+// always extends this component on every page
 
 // it caches last scroll position
-// and reverts it when activated(keep-alive)
+// and reverts it when activated
+// when keep-alive
 
 export default {
   data() {
     return {
       lastScrollPosition: 0,
       isFirstLoad: false
+    }
+  },
+  created() {
+    if (process.client) {
+      // push history to custom historyStack in store
+      // everytime load each page
+      // only push history on client side load
+      this.$store.commit('pushHistory', location.pathname)
     }
   },
   mounted() {
