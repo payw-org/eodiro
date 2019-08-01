8451c9057247a9e3cb7fd588e251a47c13344955
Jang Haemin <io@jhaemin.com>
2019-08-01 15:49:02 +0900

    Create eodiro UI Components

---
+++ src/components/ui/EodiroButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="eodiro-button" @click="$emit('click')">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+@import '~/assets/styles/scss/global-variables.scss';
+@import '~/assets/styles/scss/global-mixins.scss';
+@import '~/assets/styles/scss/eodiro-ui.scss';
+
+.eodiro-button {
+  @include background-colored;
+  color: $base-black;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem 1.5rem;
+  border-radius: $radius;
+
+  @include dark-mode {
+    color: $base-white;
+  }
+}
+</style>
---
+++ src/components/ui/EodiroInput.vue
@@ -0,0 +1,41 @@
+<template>
+  <input
+    class="eodiro-input"
+    type="text"
+    autocomplete="off"
+    spellcheck="false"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+    @keydown="$emit('keydown')"
+    @keyup="$emit('keyup')"
+    @keypress="$emit('keypress')"
+  />
+</template>
+
+<script>
+export default {
+  props: ['value']
+}
+</script>
+
+
+<style lang="scss">
+@import '~/assets/styles/scss/global-mixins.scss';
+@import '~/assets/styles/scss/eodiro-ui.scss';
+
+.eodiro-input {
+  @include bordered;
+  @include text-color;
+  background-color: transparent;
+  font-size: 1rem;
+  padding: 1rem;
+  appearance: none;
+  overflow: auto;
+  display: block;
+  width: 100%;
+
+  @include on-mobile {
+    font-size: 16px;
+  }
+}
+</style>
