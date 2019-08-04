2d464c51d5ffc2d62fe1212ec44158905ab782de
Jang Haemin <io@jhaemin.com>
2019-08-04 21:42:44 +0900

    New clubs page

---
+++ src/pages/clubs/_topic/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ $route.params.topic }}</div>
+</template>
+
+<script>
+import EodiroPageBase from '~/components/EodiroPageBase.vue'
+
+export default {
+  name: 'clubs-topic',
+  extends: EodiroPageBase,
+  meta: {
+    depth: 2
+  }
+}
+</script>
