365c583363a89228107e7557716807630d5c07dc
hchangjae <hcj@payw.org>
2019-08-14 18:52:06 +0900

    some language addition

---
+++ src/lang/search-class/en-US.js
@@ -0,0 +1,9 @@
+export default {
+  title: 'Search Class',
+  head: {
+    description: 'aaa',
+    title: 'bbb'
+  },
+  initInputText: 'Please enter Class Information',
+  buttonInnerMsg: 'Search'
+}
