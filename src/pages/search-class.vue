14ca5c9d4a9a3135053a5fa81ea14dae5680f4d7
hchangjae <hcj@payw.org>
2019-08-17 22:18:10 +0900

    build filter button ( ing )

--- src/pages/search-class.vue
+++ src/pages/search-class.vue
@@ -1,11 +1,30 @@
 <template>
   <div id="search-class">
     <div class="page-content">
-      <!-- Search Bar -->
-      <div class="search-bar">
-        <Input v-model="searchClassQuary" :placeholder="initInputText" />
-        <Button>{{ buttonInnerMsg }}</Button>
+      <!-- form-section -->
+      <div class="form-wrapper">
+        <Button class="filter-button" @click="filterIsFold = !filterIsFold">
+          {{ filterButtonMsg }}
+        </Button>
+        <div class="search-bar-wrapper">
+          <Input v-model="searchClassQuary" class="search-input" :placeholder="initInputText" />
+          <button class="search-button" />
+        </div>
       </div>
+      <!-- filter section -->
+      <div v-if="!filterIsFold" class="background" @click="filterIsFold = !filterIsFold" />
+      <transition name="filter-fold">
+        <div v-if="!filterIsFold" class="filter-container">
+          <div v-for="item in filterCategory" :key="item.title" class="filter-category-item" @click="item.isFold = !item.isFold">
+            {{ item.title }}
+          </div>
+        </div>
+      </transition>
+      <transition name="filter-fold">
+        <div v-if="!getSelectedFilterItem.isFold" class="filter-category-item-detail">
+          aaa
+        </div>
+      </transition>
     </div>
   </div>
 </template>
@@ -48,7 +67,17 @@ export default {
     return {
       searchClassQuary: '',
       initInputText: this.$t('searchClass.initInputText'),
-      buttonInnerMsg: this.$t('searchClass.buttonInnerMsg'),
+      filterButtonMsg: this.$t('searchClass.filterButtonMsg'),
+      searchButtonMsg: this.$t('searchClass.searchButtonMsg'),
+      filterIsFold: true,
+      filterCategory: [
+        { title: this.$t('searchClass.filterTitleYear'), isFold: true },
+        { title: this.$t('searchClass.filterTitleSemester'), isFold: true },
+        { title: this.$t('searchClass.filterTitleProcess'), isFold: true },
+        { title: this.$t('searchClass.filterTitleCampus'), isFold: true },
+        { title: this.$t('searchClass.filterTitleCollege'), isFold: true },
+        { title: this.$t('searchClass.filterTitleMajor'), isFold: true }
+      ],
       courseExample: {
         name: '운영체제 (영어A 강의)',
         instructor: '김성조',
@@ -73,6 +102,21 @@ export default {
       }
     }
   },
+  computed: {
+    getSelectedFilterItem () {
+      const nothingSelected = {
+        title: '',
+        isFold: true
+      }
+
+      for (let i = 0; i < this.filterCategory.length; i++) {
+        if (this.filterCategory[i].isFold === false) {
+          return this.filterCategory[i]
+        }
+      }
+      return nothingSelected
+    }
+  },
   mounted () {},
   methods: {}
 }
@@ -82,14 +126,92 @@ export default {
 @import '~/assets/styles/scss/main.scss';
 
 #search-class {
-  margin: 0 1rem;
-  .search-bar {
+  .form-wrapper {
     display: flex;
-    input {
+
+    .filter-button {
       margin-right: 1rem;
+      padding: 0 1.3rem !important;
     }
-    button {
+    .search-bar-wrapper {
+      position: relative;
+      flex-grow: 1;
+      .search-input {
+        padding-right: 2.5rem;
+      }
+      .search-button {
+        position: absolute;
+        top: 0;
+        right: 0;
+        height: 3rem;
+        width: 3rem;
+
+        @include bgImg(
+          '~assets/images/eodiro/magnifier-black.svg',
+          'center',
+          '80%'
+        );
+
+        @include dark-mode {
+          @include bgImg('~assets/images/eodiro/magnifier-white.svg');
+        }
+      }
     }
   }
+
+  .background {
+    position: fixed;
+    z-index: 159265;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .filter-container {
+    position: fixed;
+    top: $banner-height;
+    z-index: 1592653;
+
+    width: 33%;
+    max-width: $master-content-max-width/3;
+    height: calc(100vh - #{$banner-height} - 2rem);
+    border-radius: $border-radius;
+    margin: space(4) 0;
+
+    font-size: body(5);
+    background-color: #fff;
+    box-shadow: 0 0.2rem 0.7rem rgba(#000, 0.2);
+
+    @include smaller-than($width-step--1) {
+      left: 0;
+      height: calc(100vh - #{$banner-height});
+      margin: 0;
+      border-radius: 0 !important;
+    }
+    @include larger-than($width-step--2) {
+    }
+
+    .filter-category-item {
+      padding: space(4) 0;
+      margin: 0 space(4);
+      text-align: center;
+      border-top: solid;
+      @include separator;
+
+      &:first-child {
+        border-top: none;
+      }
+    }
+  }
+}
+
+.filter-fold-enter-active,
+.filter-fold-leave-active {
+  transition: opacity 0.3s;
+}
+.filter-fold-enter,
+.filter-fold-leave-to {
+  opacity: 0;
 }
 </style>
