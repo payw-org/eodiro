56ed06d571de55f403c2183dab6de281b0f15bf8
Jang Haemin <io@jhaemin.com>
2019-08-21 16:26:13 +0900

    chore: separate error messages

--- src/lang/global/en-US.js
+++ src/lang/global/en-US.js
@@ -3,6 +3,13 @@ export default {
     title: 'eodiro',
     description: 'CAU Guidance'
   },
+  error: {
+    page: {
+      firstLine: '맙소사...',
+      secondLine: '존재하지 않는 페이지에요.',
+      goHome: '홈으로 가기'
+    }
+  },
   dataFetchError:
     'Failed to load data. If this problem consistently happen, leave us an inquiry.',
   goBack: 'Go Back',
--- src/lang/global/ko-KR.js
+++ src/lang/global/ko-KR.js
@@ -3,6 +3,13 @@ export default {
     title: '어디로',
     description: '중앙대학교 길잡이'
   },
+  error: {
+    page: {
+      firstLine: 'Oh my god...',
+      secondLine: 'This page does not exist.',
+      goHome: 'Go Home'
+    }
+  },
   dataFetchError: '데이터를 가져올 수 없습니다. 문제가 지속될 시 문의해주세요.',
   goBack: '뒤로가기',
   seeAll: '전체 보기'
