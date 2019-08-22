f240ab5275a16a8dd7dfd1ad2a70d687b02b48d6
hchangjae <hcj@payw.org>
2019-08-22 19:53:40 +0900

    test is done

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,20 +1,65 @@
 <template>
-  <div />
+  <div>
+    <Grid>
+      <Button>hi</Button>
+      <Accordion>
+        <template v-slot:face>
+          <h3>Accordion</h3>
+          <p>This is accordion component.</p>
+          <p>This is accordion component.</p>
+          <p>This is accordion component.</p>
+        </template>
+
+        <template v-slot:content>
+          <p>Accordion content</p>
+        </template>
+      </Accordion>
+      <ArrowBlock>
+        <template v-slot:content>
+          <p>Menu Block</p>
+        </template>
+      </ArrowBlock>
+    </Grid>
+    <Button @click="test">
+      Test Dialog
+    </Button>
+
+    <div class="parent">
+      <div class="child" />
+    </div>
+  </div>
 </template>
 
 <script>
-// import { Accordion, ArrowBlock, Button, Grid } from '~/components/ui'
-// import Dialog from '~/plugins/eodiro-dialog'
+import { Accordion, ArrowBlock, Button, Grid } from '~/components/ui'
+import EodiroDialog from '~/plugins/eodiro-dialog'
 
 export default {
-  mounted: {
+  components: { Accordion, ArrowBlock, Button, Grid },
+  mounted () {
+    this.test()
+  },
+  methods: {
     test () {
-      // new Dialog().alert('what')
+      console.log(new EodiroDialog())
     }
   }
 }
 </script>
 
 <style lang="scss">
-@import '~/assets/styles/scss/main.scss';
+@import '~/assets/styles/scss/main';
+
+.parent {
+  width: 100px;
+  height: 100px;
+  background: black;
+  padding: 10px;
+
+  .child {
+    width: 100%;
+    height: 100%;
+    background: red;
+  }
+}
 </style>
