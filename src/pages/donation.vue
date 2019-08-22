64bcace1f917701986a4aa72dda0ea4edf8387d5
hchangjae <hcj@payw.org>
2019-08-22 15:38:25 +0900

    create new donation page

---
+++ src/pages/donation.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="donation">
+    <div class="page-content">
+      a
+    </div>
+  </div>
+</template>
+
+<script>
+import pageBase from '~/mixins/page-base'
+// import { Button, Textarea } from '~/components/ui'
+// import Dialog from '~/plugins/eodiro-dialog'
+
+export default {
+  name: 'donation',
+  // components: { Button, Textarea },
+  mixins: [pageBase],
+  head () {
+    return {
+      title: this.$t('donation.title')
+    }
+  },
+  data () {
+    return {}
+  },
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style lang="scss">
+@import '~/assets/styles/scss/main.scss';
+
+#donation {
+  max-width: 30rem !important;
+  text-align: center;
+  width: 100%;
+  height: 100%;
+
+  .page-content {
+    height: 100%;
+  }
+}
+</style>
