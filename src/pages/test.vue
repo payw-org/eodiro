03c62994d8c595073fdbd2ec256729ff1423175a
hchangjae <hcj@payw.org>
2019-08-22 16:54:50 +0900

    no answer test.vue

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,33 +1,17 @@
 <template>
-  <div>
-    <Grid>
-      <Button>hi</Button>
-      <Accordion>
-        <template v-slot:face>
-          <h3>Accordion</h3>
-          <p>This is accordion component.</p>
-          <p>This is accordion component.</p>
-          <p>This is accordion component.</p>
-        </template>
-
-        <template v-slot:content>
-          <p>Accordion content</p>
-        </template>
-      </Accordion>
-      <ArrowBlock>
-        <template v-slot:content>
-          <p>Menu Block</p>
-        </template>
-      </ArrowBlock>
-    </Grid>
-  </div>
+  <div />
 </template>
 
 <script>
-import { Accordion, ArrowBlock, Button, Grid } from '~/components/ui'
+// import { Accordion, ArrowBlock, Button, Grid } from '~/components/ui'
+// import Dialog from '~/plugins/eodiro-dialog'
 
 export default {
-  components: { Accordion, ArrowBlock, Button, Grid }
+  mounted: {
+    test () {
+      // new Dialog().alert('what')
+    }
+  }
 }
 </script>
 
