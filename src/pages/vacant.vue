a2699c0163a234d3d473b53849b71247adfa3984
Jang Haemin <io@jhaemin.com>
2019-08-25 01:58:20 +0900

    Move badge styles from global to vacant scope

--- src/pages/vacant.vue
+++ src/pages/vacant.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="eodiro-vacant">
-    <nuxt-child keep-alive :keep-alive-props="{ include: $store.state.cachedComponents }" />
+    <nuxt-child
+      keep-alive
+      :keep-alive-props="{ include: $store.state.cachedComponents }"
+    />
   </div>
 </template>
 
@@ -12,3 +15,36 @@ export default {
   mixins: [pageBase]
 }
 </script>
+
+<style lang="scss">
+@import '~/assets/styles/scss/main';
+
+#eodiro-vacant {
+  .empty-count-badge {
+    white-space: nowrap;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1rem;
+    color: $c-step--4;
+    background-color: $t-black;
+    border-radius: radius(3);
+    padding: 0 0.5rem;
+    min-height: 2rem;
+    min-width: 2rem;
+    overflow: hidden;
+    @include overlay-inverted;
+
+    &,
+    & * {
+      font-weight: 500;
+    }
+
+    .total {
+      opacity: 0.6;
+      font-size: 0.8em;
+    }
+  }
+}
+</style>
