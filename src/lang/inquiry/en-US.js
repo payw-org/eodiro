2c0ba46f48f15089135324dbe8e7f992bc5b095e
Jang Haemin <io@jhaemin.com>
2019-08-26 15:14:54 +0900

    chore: add phrases

--- src/lang/inquiry/en-US.js
+++ src/lang/inquiry/en-US.js
@@ -1,5 +1,10 @@
 export default {
   title: 'Inquiry',
   send: 'Send',
-  writeHere: 'Write anything here.'
+  writeHere: 'Write anything here.',
+  sent: 'Your inquiry has been sent!',
+  textLimit: {
+    under: 'You have to write at least one charater',
+    over: '500 characters limit'
+  }
 }
--- src/lang/inquiry/ko-KR.js
+++ src/lang/inquiry/ko-KR.js
@@ -1,5 +1,10 @@
 export default {
   title: '문의하기',
   send: '전송',
-  writeHere: '이 곳에 내용을 입력하세요.'
+  writeHere: '이 곳에 내용을 입력하세요.',
+  sent: '전송되었습니다!',
+  textLimit: {
+    under: '한 글자 이상 입력해주세요',
+    over: '500글자까지만 보낼 수 있습니다.'
+  }
 }
