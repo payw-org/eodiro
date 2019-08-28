5fb5027ac29091718e52ef6f39ee26eee6b89fe6
Jang Haemin <io@jhaemin.com>
2019-08-28 22:53:19 +0900

    Testing icon components

---
+++ src/components/ui/icons/IconArrowLeft.vue
@@ -0,0 +1,26 @@
+<template>
+  <img src="~assets/images/arrow_right_gray.svg" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import icon from '~/mixins/icon'
+
+export default Vue.extend({
+  mixins: [icon]
+})
+</script>
+
+<style lang="scss">
+.eodiro-icon {
+  display: inline-block;
+  &.arrow-top {
+  }
+  &.arrow-right {
+  }
+  &.arrow-bottom {
+  }
+  &.arrow-left {
+  }
+}
+</style>
