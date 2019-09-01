b12bc7956dcf7f3e5943e2ecf2456264626a077a
Jang Haemin <io@jhaemin.com>
2019-09-01 21:59:04 +0900

    Build new layouts

--- src/pages/meals/index.vue
+++ src/pages/meals/index.vue
@@ -12,36 +12,124 @@
       </button>
     </div>
 
-    <div class="meals-container">
-      <Accordion class="agony">
-        <template v-slot:face>
-          <h3>{{ $t('meals.breakfast') }}</h3>
-        </template>
-      </Accordion>
+    <section class="meal-time-group">
+      <h1 class="meal-time-name">
+        {{ $t('meals.breakfast') }}
+      </h1>
+      <Grid class="restaurant-container">
+        <Accordion class="restaurant-item">
+          <template v-slot:face>
+            <h2 class="restaurant-name">
+              참슬기식당(310관 B4층)
+            </h2>
+          </template>
+        </Accordion>
+        <Accordion class="restaurant-item">
+          <template v-slot:face>
+            <h2 class="restaurant-name">
+              학생식당(303관 B1층)
+            </h2>
+          </template>
+        </Accordion>
+        <Accordion class="restaurant-item">
+          <template v-slot:face>
+            <h2 class="restaurant-name">
+              생활관식당(블루미르 308관)
+            </h2>
+          </template>
+        </Accordion>
+        <Accordion class="restaurant-item">
+          <template v-slot:face>
+            <h2 class="restaurant-name">
+              University Club(102관 11층)
+            </h2>
+          </template>
+        </Accordion>
+      </Grid>
+    </section>
 
-      <Accordion class="agony">
-        <template v-slot:face>
-          <h3>{{ $t('meals.lunch') }}</h3>
-        </template>
-      </Accordion>
+    <section class="meal-time-group">
+      <h1 class="meal-time-name">
+        {{ $t('meals.lunch') }}
+      </h1>
+      <Grid class="restaurant-container">
+        <Accordion class="restaurant-item">
+          <template v-slot:face>
+            <h2 class="restaurant-name">
+              참슬기식당(310관 B4층)
+            </h2>
+          </template>
+        </Accordion>
+        <Accordion class="restaurant-item">
+          <template v-slot:face>
+            <h2 class="restaurant-name">
+              학생식당(303관 B1층)
+            </h2>
+          </template>
+        </Accordion>
+        <Accordion class="restaurant-item">
+          <template v-slot:face>
+            <h2 class="restaurant-name">
+              생활관식당(블루미르 308관)
+            </h2>
+          </template>
+        </Accordion>
+        <Accordion class="restaurant-item">
+          <template v-slot:face>
+            <h2 class="restaurant-name">
+              University Club(102관 11층)
+            </h2>
+          </template>
+        </Accordion>
+      </Grid>
+    </section>
 
-      <Accordion class="agony">
-        <template v-slot:face>
-          <h3>{{ $t('meals.supper') }}</h3>
-        </template>
-      </Accordion>
-    </div>
+    <section class="meal-time-group">
+      <h1 class="meal-time-name">
+        {{ $t('meals.supper') }}
+      </h1>
+      <Grid class="restaurant-container">
+        <Accordion class="restaurant-item">
+          <template v-slot:face>
+            <h2 class="restaurant-name">
+              참슬기식당(310관 B4층)
+            </h2>
+          </template>
+        </Accordion>
+        <Accordion class="restaurant-item">
+          <template v-slot:face>
+            <h2 class="restaurant-name">
+              학생식당(303관 B1층)
+            </h2>
+          </template>
+        </Accordion>
+        <Accordion class="restaurant-item">
+          <template v-slot:face>
+            <h2 class="restaurant-name">
+              생활관식당(블루미르 308관)
+            </h2>
+          </template>
+        </Accordion>
+        <Accordion class="restaurant-item">
+          <template v-slot:face>
+            <h2 class="restaurant-name">
+              University Club(102관 11층)
+            </h2>
+          </template>
+        </Accordion>
+      </Grid>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import pageBase from '~/mixins/page-base'
-import { Accordion } from '~/components/ui'
+import { Accordion, Grid } from '~/components/ui'
 
 export default Vue.extend({
   name: 'meals-index',
-  components: { Accordion },
+  components: { Accordion, Grid },
   mixins: [pageBase]
 })
 </script>
@@ -51,19 +139,21 @@ export default Vue.extend({
 
 #eodiro-meals {
   .date-picker {
-    display: flex;
     align-items: center;
+    border-radius: 50px;
+    display: flex;
     justify-content: space-around;
     @include elm-fill;
-    border-radius: radius(2);
-    padding: space(2);
-    margin-bottom: space(5);
+    margin: auto;
+    margin-bottom: space(3);
+    max-width: 20rem;
 
     button {
       flex: 1;
-      min-height: 2rem;
       @include text-color;
       font-weight: fw(4);
+      margin: space(2) 0;
+      padding: space(1) 0;
     }
 
     .today {
@@ -73,9 +163,33 @@ export default Vue.extend({
     }
   }
 
-  .meals-container {
-    .agony {
-      margin-top: space(4);
+  .meal-time-group {
+    margin-bottom: space(6);
+    padding-bottom: space(6);
+    @include separator(bottom);
+
+    &:last-child {
+      padding-bottom: 0;
+      margin-bottom: 0;
+      border: none;
+    }
+
+    .meal-time-name {
+      font-size: head(3);
+      padding: space(3) 0;
+      padding-left: radius(1);
+      position: sticky;
+      top: $nav-height;
+      z-index: 2;
+      @include bg;
+    }
+
+    .restaurant-item {
+      .restaurant-name {
+        color: #529dff;
+        font-size: body(5);
+        font-weight: fw(4);
+      }
     }
   }
 }
