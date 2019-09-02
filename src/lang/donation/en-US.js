1346ed6a8830ff7b526cefaf433b5f226e09c686
Jang Haemin <io@jhaemin.com>
2019-09-02 21:16:08 +0900

    Test the hall of fame

--- src/lang/donation/en-US.js
+++ src/lang/donation/en-US.js
@@ -2,5 +2,9 @@ export default {
   title: 'Donate',
   bankName: 'Kakao Bank',
   pressToCopy: 'Copy to clipboard',
-  copied: 'Copied to clipboard.'
+  copied: 'Copied to clipboard.',
+  hallOfFame: 'The Hall of Fame',
+  hallOfFameDesc: 'These are donator',
+  manifesto:
+    'We started this open source project, eodiro, to give you a better experience, to make you feel love and pride toward our university. Support us by contributing to our repository at GitHub or you can donate to help us maintain this project.'
 }
--- src/pages/donation.vue
+++ src/pages/donation.vue
@@ -1,34 +1,83 @@
 <template>
   <div id="donation">
     <div class="page-content">
-      <!-- <div class="kakaobank-account" @click="copyToClipboard" /> -->
-      <div class="card" @click="copyToClipboard">
-        <div class="card-info">
-          <div class="bank">
-            <img
-              class="bank-logo"
-              src="~/assets/images/kakaobank-logo.svg"
-              alt=""
-              data-color-scheme="light"
-            />
-            <img
-              class="bank-logo"
-              src="~/assets/images/kakaobank-logo-yellow.svg"
-              alt=""
-              data-color-scheme="dark"
-            />
-            <h1 class="bank-name">
-              {{ $t('donation.bankName') }}
-            </h1>
+      <div class="manifesto-and-card">
+        <p class="manifesto">
+          {{ $t('donation.manifesto') }}
+        </p>
+        <div class="card" @click="copyToClipboard">
+          <div class="card-info">
+            <div class="bank">
+              <img
+                class="bank-logo"
+                src="~/assets/images/kakaobank-logo.svg"
+                alt=""
+                data-color-scheme="light"
+              />
+              <img
+                class="bank-logo"
+                src="~/assets/images/kakaobank-logo-yellow.svg"
+                alt=""
+                data-color-scheme="dark"
+              />
+              <h1 class="bank-name">
+                {{ $t('donation.bankName') }}
+              </h1>
+            </div>
+            <div class="card-body">
+              <span class="press-to-copy">
+                {{ $t('donation.pressToCopy') }}
+              </span>
+              <h2 class="bank-account-number">
+                {{ accountNumber }}
+              </h2>
+            </div>
           </div>
-          <div class="card-body">
-            <span class="press-to-copy">
-              {{ $t('donation.pressToCopy') }}
-            </span>
-            <h2 class="bank-account-number">
-              {{ accountNumber }}
-            </h2>
-          </div>
+        </div>
+      </div>
+
+      <div class="the-hall-of-fame-wrapper">
+        <div class="thof">
+          <h1 class="title">
+            {{ $t('donation.hallOfFame') }}
+          </h1>
+          <ol class="donator-list">
+            <li class="donator-item">
+              <div class="identity">
+                <span class="num-badge">1</span>
+                황창재
+              </div>
+              <span class="amount">₩12억</span>
+            </li>
+            <li class="donator-item">
+              <div class="identity">
+                <span class="num-badge">2</span>
+                장해민
+              </div>
+              <span class="amount">₩10억</span>
+            </li>
+            <li class="donator-item">
+              <div class="identity">
+                <span class="num-badge">3</span>
+                황치훈
+              </div>
+              <span class="amount">₩5억</span>
+            </li>
+            <li class="donator-item">
+              <div class="identity">
+                <span class="num-badge">4</span>
+                우승진
+              </div>
+              <span class="amount">₩1억</span>
+            </li>
+            <li class="donator-item">
+              <div class="identity">
+                <span class="num-badge">5</span>
+                이성민
+              </div>
+              <span class="amount">₩50,000,000</span>
+            </li>
+          </ol>
         </div>
       </div>
     </div>
@@ -77,93 +126,152 @@ export default {
 
 #donation {
   text-align: center;
+  // max-width: 50rem !important;
 
   .page-content {
     display: flex;
-    align-items: center;
+    align-items: flex-start;
     justify-content: center;
 
-    .kakaobank-account {
-      width: 10rem;
-      height: 10rem;
-      border-radius: 1rem;
-      margin: 8vh 0;
-      @include larger-than($width-step--1) {
-        width: 13rem;
-        height: 13rem;
+    @include on-mobile {
+      flex-direction: column;
+    }
 
-        border-radius: 2rem;
-        margin: 5rem 0;
+    .manifesto-and-card {
+      flex: 1;
+      display: flex;
+      justify-content: center;
+      flex-wrap: wrap;
+      margin-right: space(5);
+
+      @include on-mobile {
+        margin-right: 0;
+        margin-bottom: far(2);
       }
-      @include bgImg('~assets/images/kakaobank-account-white.svg', center);
 
-      @include dark-mode {
-        @include bgImg('~assets/images/kakaobank-account-white.svg', center);
+      .manifesto {
+        min-width: 100%;
+        text-align: left;
+        margin-bottom: space(4);
+        padding: 0 radius(2);
+      }
+
+      .card {
+        position: relative;
+        background-color: #ffe600;
+        color: #281d20;
+        border-radius: radius(3);
+        width: 100%;
+        max-width: 22rem;
+        cursor: pointer;
+
+        &::before {
+          content: '';
+          display: block;
+          padding-top: 60%;
+        }
+
+        @include dark-mode {
+          background-color: #33272a;
+          color: #ffe600;
+        }
+
+        .card-info {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          padding: space(5);
+
+          .bank {
+            display: flex;
+            align-items: flex-end;
+
+            .bank-logo {
+              height: head(1);
+              margin-right: space(2);
+            }
+
+            .bank-name {
+              font-size: head(1);
+              line-height: lh(1);
+            }
+          }
+
+          .card-body {
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+            align-items: center;
+            justify-content: center;
+
+            .press-to-copy {
+              font-size: body(2);
+              margin-bottom: space(3);
+              color: $base-gray;
+            }
+
+            .bank-account-number {
+              font-size: head(1);
+              @include overlay-inverted;
+              padding: space(2);
+              border-radius: radius(2);
+            }
+          }
+        }
       }
     }
-  }
 
-  .card {
-    position: relative;
-    background-color: #ffe600;
-    color: #281d20;
-    border-radius: radius(3);
-    width: 100%;
-    max-width: 20rem;
-    cursor: pointer;
-
-    &::before {
-      content: '';
-      display: block;
-      padding-top: 60%;
-    }
-
-    @include dark-mode {
-      background-color: #33272a;
-      color: #ffe600;
-    }
-
-    .card-info {
-      position: absolute;
-      top: 0;
-      left: 0;
+    .the-hall-of-fame-wrapper {
+      flex: 1;
       width: 100%;
-      height: 100%;
-      padding: space(5);
 
-      .bank {
-        display: flex;
-        align-items: flex-end;
+      .thof {
+        text-align: left;
+        @include elm-fill;
+        border-radius: radius(5);
+        padding: space(4);
 
-        .bank-logo {
-          height: head(1);
-          margin-right: space(2);
+        .title {
+          font-size: head(2);
+          padding-left: radius(1);
         }
 
-        .bank-name {
-          font-size: head(1);
-          line-height: lh(1);
-        }
-      }
+        .donator-list {
+          .donator-item {
+            @include bg;
+            border-radius: radius(3);
+            padding: space(3);
+            margin-top: space(4);
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
 
-      .card-body {
-        display: flex;
-        flex-direction: column;
-        height: 100%;
-        align-items: center;
-        justify-content: center;
+            .identity {
+              display: flex;
+              align-items: center;
+            }
 
-        .press-to-copy {
-          font-size: body(2);
-          margin-bottom: space(3);
-          color: $base-gray;
-        }
+            .num-badge {
+              background-color: #8c00ff;
+              font-weight: fw(5);
+              font-size: body(2);
+              color: #fff;
+              border-radius: 50px;
+              width: 1.5rem;
+              height: 1.5rem;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              margin-right: space(3);
+            }
 
-        .bank-account-number {
-          font-size: head(1);
-          @include overlay-inverted;
-          padding: space(2);
-          border-radius: radius(2);
+            .amount {
+              font-size: body(2);
+              color: #0077ff;
+            }
+          }
         }
       }
     }
