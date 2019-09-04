1ffa875bb67a93a770d3eb303f4996cd14a450e2
Jang Haemin <io@jhaemin.com>
2019-09-04 15:12:59 +0900

    New phrases

--- src/lang/community/ko-KR.js
+++ src/lang/community/ko-KR.js
@@ -4,5 +4,11 @@ export default {
   comments: '댓글',
   noComments: '댓글이 없습니다',
   leaveComment: '내용을 입력하세요',
-  newPost: '새 포스트'
+  new: {
+    title: '새 포스트',
+    placeholder: {
+      title: '제목',
+      body: '본문'
+    }
+  }
 }
