9d8a63edcf567a6ab5341a61fdec88a7ed1b5719
Jang Haemin <io@jhaemin.com>
2019-09-10 09:18:30 +0900

    Add phrases

--- src/lang/inquiry/ko-KR.js
+++ src/lang/inquiry/ko-KR.js
@@ -1,11 +1,13 @@
 export default {
   title: '문의하기',
   send: '전송',
-  inputEmail: '(선택) 답장 받을 이메일',
+  inputEmail: '답장 받을 이메일',
   writeHere: '내용을 입력하세요',
   sent: '전송되었습니다!',
+  requireEmail: '이메일을 입력해주세요',
+  wrongEmailFormat: '이메일 형식이 잘못됐습니다',
   textLimit: {
-    under: '한 글자 이상 입력해주세요',
+    under: '두 글자 이상 입력해주세요',
     over: '500글자까지만 보낼 수 있습니다.'
   }
 }
