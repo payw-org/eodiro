935620c360496919ee1bb2da68f3533ef69fc93d
Jang Haemin <io@jhaemin.com>
2019-09-26 15:11:04 +0900

    Add opensource page information

---
+++ src/lang/opensource/en-US.js
@@ -0,0 +1,6 @@
+export default {
+  title: 'Open Source',
+  manifesto:
+    'eodiro is an open source which is readable and editable by anyone interested in this project.',
+  visit: 'Visit GitHub'
+}
--- src/pages/opensource.vue
+++ src/pages/opensource.vue
@@ -1,6 +1,38 @@
 <template>
   <div id="opensource">
-    <h1>Open Source</h1>
+    <h1>{{ $t('opensource.title') }}</h1>
+    <p class="manifesto">
+      {{ $t('opensource.manifesto') }}
+    </p>
+
+    <div class="github-star-button">
+      <a
+        class="github-button"
+        href="https://github.com/paywteam/eodiro"
+        data-icon="octicon-star"
+        data-size="large"
+        data-show-count="true"
+        aria-label="Star paywteam/eodiro on GitHub"
+      >
+        Star
+      </a>
+    </div>
+
+    <button class="github-organization-button">
+      <a
+        href="https://github.com/paywteam/eodiro"
+        class="absolute-link"
+        target="_blank"
+      />
+      <span class="github-logo-wrapper">
+        <img
+          src="@/assets/images/github/logo-inverted.svg"
+          alt="github logo"
+          class="github-logo"
+        />
+      </span>
+      <span>{{ $t('opensource.visit') }}</span>
+    </button>
   </div>
 </template>
 
@@ -10,6 +42,55 @@ import pageBase from '~/mixins/page-base'
 
 export default Vue.extend({
   name: 'opensource',
-  mixins: [pageBase]
+  mixins: [pageBase],
+  head() {
+    return {
+      script: [
+        {
+          async: true,
+          defer: true,
+          src: 'https://buttons.github.io/buttons.js'
+        }
+      ]
+    }
+  }
 })
 </script>
+
+<style lang="scss">
+@import '@/assets/styles/scss/main';
+
+#opensource {
+  .manifesto {
+    margin-top: space(2);
+  }
+
+  .github-star-button {
+    margin-top: space(4);
+  }
+
+  .github-organization-button {
+    @include bg-inverted;
+    @include text-color-inverted;
+    padding: space(2) space(3);
+    margin-top: space(3);
+    border-radius: radius(2);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+
+    .github-logo-wrapper {
+      margin-right: space(2);
+      width: 1.2rem;
+      height: 1.2rem;
+      display: flex;
+
+      .github-logo {
+        width: 100%;
+        height: auto;
+      }
+    }
+  }
+}
+</style>
