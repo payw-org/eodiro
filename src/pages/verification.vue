177f54f4d56cbc9c6d9be92f9163b945b5011f95
Jang Haemin <io@jhaemin.com>
2019-10-25 21:24:15 +0900

    test: verification

---
+++ src/pages/verification.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="eodiro-verification" />
+</template>
+
+<script>
+// import Axios from 'axios'
+
+export default {
+  middleware({ app, route, redirect }) {
+    const { t } = route.query
+    if (!t) {
+      redirect(app.localePath('home'))
+    }
+
+    const token = t
+    console.log(token)
+    // Axios.get('https://api2.eodiro.com/')
+  }
+}
+</script>
