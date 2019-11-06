b1437f17220dcd6196909a8cb10bf84977dbcdc5
Jang Haemin <io@jhaemin.com>
2019-11-06 17:43:25 +0900

    feat: SPA redirection

--- src/plugins/init.js
+++ src/plugins/init.js
@@ -26,11 +26,9 @@ function getRedirectPath(currentPath, lang) {
  * @param {import('@nuxt/types').Context} context
  */
 export default (context) => {
-  const { route, store, redirect, app } = context
+  const { store, app, route, redirect } = context
 
-  // Color scheme init
-  const userColorScheme = JsCookie.get(colorSchemeCookieName)
-  store.commit('SET_COLOR_SCHEME', userColorScheme)
+  const pathnameQueryString = location.pathname + location.search
 
   // Language init
   const redirectLang = JsCookie.get(langCookieName)
@@ -42,29 +40,71 @@ export default (context) => {
       .toLocaleLowerCase()
 
     if (browserLang === 'ko') {
-      redirect(getRedirectPath(route.path, 'kr'))
+      redirect(getRedirectPath(pathnameQueryString, 'kr'))
     } else {
-      redirect(getRedirectPath(route.path, 'en'))
+      redirect(getRedirectPath(pathnameQueryString, 'en'))
     }
-
-    return
   }
 
   // Language cookie available
   const routeName = route.name
-  if (route.name !== undefined) {
+  if (routeName !== undefined) {
     if (routeName.endsWith('___en')) {
       // Visit english url page
       if (redirectLang !== 'en') {
         // But cookie is set to Korean
-        window.location.replace(getRedirectPath(route.path, 'kr'))
+        window.location.replace(getRedirectPath(pathnameQueryString, 'kr'))
       }
     } else if (redirectLang === 'en') {
       // Route is Korean
       // but cookie is set to english
-      window.location.replace(getRedirectPath(route.path, 'en'))
+      window.location.replace(getRedirectPath(pathnameQueryString, 'en'))
     }
   }
+
+  // Single Page Apps for GitHub Pages
+  // https://github.com/rafrex/spa-github-pages
+  // Copyright (c) 2016 Rafael Pedicini, licensed under the MIT License
+  // ----------------------------------------------------------------------
+  // This script checks to see if a redirect is present in the query string
+  // and converts it back into the correct url and adds it to the
+  // browser's history using window.history.replaceState(...),
+  // which won't cause the browser to attempt to load the new url.
+  // When the single page app is loaded further down in this file,
+  // the correct url will be waiting in the browser's history for
+  // the single page app to route accordingly.
+  (function(l) {
+    if (l.search) {
+      var q = {}
+      l.search
+        .slice(1)
+        .split('&')
+        .forEach(function(v) {
+          var a = v.split('=')
+          q[a[0]] = a
+            .slice(1)
+            .join('=')
+            .replace(/~and~/g, '&')
+        })
+
+      // Find the key "p"
+      // /?p=
+      if (q.p !== undefined) {
+        window.history.replaceState(
+          null,
+          null,
+          l.pathname.slice(0, -1) +
+            (q.p || '') +
+            (q.q ? '?' + q.q : '') +
+            l.hash
+        )
+      }
+    }
+  })(window.location)
+
+  // Color scheme init
+  const userColorScheme = JsCookie.get(colorSchemeCookieName)
+  store.commit('SET_COLOR_SCHEME', userColorScheme)
 
   // Polyfills
   require('~/polyfills')
