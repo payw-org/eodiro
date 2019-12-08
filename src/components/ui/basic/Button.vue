a14c1b4220e493116159f9f35bdc15a5b4884ef9
Jang Haemin <io@jhaemin.com>
2019-12-09 03:21:46 +0900

    Support multi-line button layout

--- src/components/ui/basic/Button.vue
+++ src/components/ui/basic/Button.vue
@@ -22,11 +22,11 @@ export default {
 .btn {
   @include elm-fill;
   color: $base-black;
-  height: $btn-height !important;
+  min-height: $btn-height !important;
   display: flex;
   align-items: center;
   justify-content: center;
-  padding: 0 1.5rem !important;
+  padding: 0.5rem 1rem !important;
   border-radius: r(4) !important;
   cursor: pointer;
 
