75525a46a88afcd248a753a886b31eefebb2f5bd
Jang Haemin <io@jhaemin.com>
2019-12-21 22:12:04 +0900

    fix: set correct api2 url

--- src/modules/eodiro-api/api-host.js
+++ src/modules/eodiro-api/api-host.js
@@ -4,10 +4,15 @@ export default class ApiHost {
    * @param {string} host
    */
   static setHost(host) {
-    if (host.includes('eodiro')) {
+    if (host.startsWith('eodiro')) {
+      // eodiro.com
       host = 'https://api2.eodiro.com'
+    } else if (host.startsWith('dev')) {
+      // dev.eodiro.com
+      host = 'https://dev.api2.eodiro.com'
     } else {
-      host = 'http://' + host.split(':')[0] + ':4000'
+      // Dev
+      host = 'http://' + host.split(':')[0] + ':4020'
     }
 
     this.host = host
