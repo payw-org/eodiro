6454d8c153556b7a2f74e89df96b1f4b0e51ac6b
Jang Haemin <io@jhaemin.com>
2019-12-26 00:13:23 +0900

    feat: upgrade the ways checking authentication

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -4,47 +4,85 @@ import { AuthApi } from '~/modules/eodiro-api'
 
 export default class Auth {
   /**
+   * Verify access token from server
+   * then refresh tokens if it is necessary.
+   *
    * @param {Http=} http
    * @returns {Promise<boolean>}
    */
-  static async isSignedIn(http) {
-    if (!this.getAccessToken(http)) {
-      return false
-    }
-
+  static async isSignedInRefresh(http) {
+    // Verify from the server if tokens exists
+    // It may fail due to 2 reasons
+    // 1. No access token on the device
+    // 2. Access token has been expired
     const authApi = new AuthApi(http)
-
     const isSignedIn = await authApi.isSignedIn()
     if (isSignedIn) {
+      this.updateLastlySignedInVerificationTime()
       return true
     }
 
+    // It will fail when refresh token has been expired
     const tokens = await authApi.refreshTokens()
     if (!tokens) {
+      this.signedInFlag = false
+      this.lastlySignedInVerificationTime = null
       return false
     }
 
+    // Set tokens with refreshed ones
     this.setJwt(tokens.accessToken, tokens.refreshToken, http)
+    this.updateLastlySignedInVerificationTime()
 
     return true
   }
 
   /**
-   * @param {Http=} http
-   * @returns {string}
+   * Shallow check if an user is signed in
+   * by looking at the application-wide static flag.
    */
-  static getAccessToken(http) {
-    const tokens = this.getTokens(http)
-    return tokens ? tokens.accessToken : undefined
+  static isSignedInQuick() {
+    return this.signedInFlag
+  }
+
+  /**
+   * @param {Http} http
+   * @returns {Promise<boolean>}
+   */
+  static async isSignedInCritical(http) {
+    if (!this.isSignedInQuick() || !this.lastlySignedInVerificationTime) {
+      return false
+    }
+
+    if (this.lastlySignedInVerificationTime.diff(dayjs().minute() > 60)) {
+      const result = await this.isSignedInRefresh(http)
+      return result
+    } else {
+      return this.isSignedInQuick()
+    }
+  }
+
+  static updateLastlySignedInVerificationTime() {
+    this.lastlySignedInVerificationTime = dayjs()
+    this.signedInFlag = true
   }
 
   /**
    * @param {Http=} http
-   * @returns {string}
+   * @returns {string|null}
+   */
+  static getAccessToken(http) {
+    const tokens = this.getTokens(http)
+    return tokens ? tokens.accessToken : null
+  }
+
+  /**
+   * @param {Http=} http
+   * @returns {string|null}
    */
   static getRefreshToken(http) {
     const tokens = this.getTokens(http)
-    return tokens ? tokens.refreshToken : undefined
+    return tokens ? tokens.refreshToken : null
   }
 
   /**
@@ -74,6 +112,9 @@ export default class Auth {
         .toDate(),
       path: '/'
     })
+
+    this.updateLastlySignedInVerificationTime()
+    this.signedInFlag = true
   }
 
   /**
@@ -82,5 +123,11 @@ export default class Auth {
   static clearJwt(http) {
     const eodiroCookie = new EodiroCookie(http)
     eodiroCookie.remove('tokens')
+
+    this.lastlySignedInVerificationTime = null
+    this.signedInFlag = false
   }
 }
+
+Auth.lastlySignedInVerificationTime = null
+Auth.signedInFlag = false
