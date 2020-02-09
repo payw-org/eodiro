483464d3e6f88ca5d371b27e9db1a0af8fac9626
Jang Haemin <io@jhaemin.com>
2020-02-09 18:21:31 -0500

    chore: lower the saturation of green color scheme

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -261,10 +261,10 @@ export default {
       }
 
       .icon--lectures {
-        background-image: url('~assets/images/home/home_menu_icon_search-class.svg');
+        background-image: url('~assets/images/home/home_menu_icon_class.svg');
 
         @include dark-mode {
-          background-image: url('~assets/images/home/home_menu_icon_search-class_black.svg');
+          background-image: url('~assets/images/home/home_menu_icon_class_black.svg');
         }
       }
 
