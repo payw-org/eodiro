ec76cc6ded744cb672a18856eaa6646f256480a2
Jang Haemin <io@jhaemin.com>
2020-02-10 21:14:25 -0500

    feat: search lectures

--- src/modules/eodiro-api/lecture-api.js
+++ src/modules/eodiro-api/lecture-api.js
@@ -24,4 +24,28 @@ export default class LectureApi extends Api {
 
     return err ? false : res.data
   }
+
+  /**
+   * @param {Object} options
+   * @param {number} options.year
+   * @param {string} options.semester
+   * @param {string} options.campus
+   * @param {number} options.amount
+   * @param {number} options.offset
+   * @param {string} options.query
+   */
+  async searchLectures(options) {
+    const { year, semester, campus, amount, offset, query } = options
+    const [err, res] = await useAxios({
+      method: 'get',
+      url: ApiHost.getUrl(`${year}/${semester}/${campus}/lectures/search`),
+      params: {
+        amount,
+        offset,
+        q: query,
+      },
+    })
+
+    return err ? false : res.data
+  }
 }
