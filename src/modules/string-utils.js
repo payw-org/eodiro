6fed69336a9f88ef7d5177d3848a2ce8b55144cb
Jang Haemin <io@jhaemin.com>
2020-02-14 12:02:47 -0500

    feat: convert camelCase to kebab-case

---
+++ src/modules/string-utils.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} str
+ */
+export const camelToKebab = function(str) {
+  return str
+    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+    .replace(/([A-Z])([A-Z])(?=[a-z])/g, '$1-$2')
+    .toLowerCase()
+}
