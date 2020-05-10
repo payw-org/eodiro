e147b5e20cc37506df0b18b2958fcea6c7d02b37
Jang Haemin <io@jhaemin.com>
2020-05-10 20:48:56 +0900

    config: add typescript-eslint/eslint-recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     },
   },
   extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:prettier/recommended',
