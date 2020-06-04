4f42bebbbd8ffc1b6fa70ba2b7f07d1317059f2a
jhaemin <io@jhaemin.com>
2020-06-04 21:43:45 +0900

    feat: notice notifications

---
+++ src/pages/notice-notifications.tsx
@@ -0,0 +1,28 @@
+import { GetServerSideProps, NextPage } from 'next'
+import NoticeNotifications, {
+  NoticeNotificationsProps,
+} from '@/components/notice-notifications/NoticeNotifications'
+
+import ApiHost from '@/modules/api-host'
+import { oneAPIClient } from '@payw/eodiro-one-api'
+
+export const getServerSideProps: GetServerSideProps<NoticeNotificationsProps> = async () => {
+  const { data } = await oneAPIClient(ApiHost.getHost(), {
+    action: 'getNoticeCatalog',
+    data: undefined,
+  })
+
+  const { noticeCatalog } = data
+
+  return {
+    props: {
+      noticeCatalog,
+    },
+  }
+}
+
+const Page: NextPage<NoticeNotificationsProps> = ({ noticeCatalog }) => {
+  return <NoticeNotifications noticeCatalog={noticeCatalog} />
+}
+
+export default Page
