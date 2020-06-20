15b245d26f853b1f19a0dc6ddd878c77b26dfc8f
jhaemin <io@jhaemin.com>
2020-06-20 19:41:44 +0900

    feat: display no item message

--- src/components/tips/TipsList/index.tsx
+++ src/components/tips/TipsList/index.tsx
@@ -1,6 +1,7 @@
 import $ from './style.module.scss'
 import TipItem from './TipItem'
 import { TipListResponse } from '@payw/eodiro-one-api/database/models/tip'
+import classNames from 'classnames'
 
 export type TipsListProps = {
   tipsData: TipListResponse[]
@@ -9,9 +10,14 @@ export type TipsListProps = {
 const TipsList: React.FC<TipsListProps> = ({ tipsData: tips }) => {
   return (
     <div className={$['tips-list']}>
-      {tips.map((tip, i) => (
-        <TipItem key={i} tipData={tip} />
-      ))}
+      {tips.length > 0 ? (
+        tips.map((tip, i) => <TipItem key={i} tipData={tip} />)
+      ) : (
+        <div className={$['empty']}>
+          <i className={classNames('f7-icons', $['icon'])}>tray_fill</i>
+          <span className={$['msg']}>아직 꿀팁이 없습니다.</span>
+        </div>
+      )}
     </div>
   )
 }
