69f4b54438bb8ca29b326770a393c434399f9415
jhaemin <io@jhaemin.com>
2020-06-21 12:16:52 +0900

    feat: display spinner while loading data

--- src/components/tips/TipsList/index.tsx
+++ src/components/tips/TipsList/index.tsx
@@ -1,16 +1,22 @@
 import $ from './style.module.scss'
+import { Spinner } from '@/components/global/Spinner'
 import TipItem from './TipItem'
 import { TipListResponse } from '@payw/eodiro-one-api/database/models/tip'
 import classNames from 'classnames'
 
 export type TipsListProps = {
   tipsData: TipListResponse[]
+  isLoading: boolean
 }
 
-const TipsList: React.FC<TipsListProps> = ({ tipsData: tips }) => {
+const TipsList: React.FC<TipsListProps> = ({ tipsData: tips, isLoading }) => {
   return (
     <div className={$['tips-list']}>
-      {tips.length > 0 ? (
+      {isLoading ? (
+        <div className={$['is-loading']}>
+          <Spinner />
+        </div>
+      ) : tips.length > 0 ? (
         tips.map((tip, i) => <TipItem key={i} tipData={tip} />)
       ) : (
         <div className={$['empty']}>
