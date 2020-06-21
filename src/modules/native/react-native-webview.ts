998dc94c35204fdd090cb67a757a003d9d663725
jhaemin <io@jhaemin.com>
2020-06-21 13:35:53 +0900

    fix: pass only a single argument to ReactNativeWebView.postMessage()

--- src/modules/native/react-native-webview.ts
+++ src/modules/native/react-native-webview.ts
@@ -4,23 +4,16 @@ import ApiHost from '../api-host'
  * Post a message to the React Native WebView
  */
 export const reactNativeWebViewPostMessage = (
-  message: Record<string, unknown>,
-  /**
-   * @default `location.origin`
-   */
-  targetOrigin?: string,
-  transfer?: Transferable[]
+  message: Record<string, unknown>
 ): void => {
   if (typeof window !== undefined && 'ReactNativeWebView' in window) {
-    const reactNativeWebView = (window as any).ReactNativeWebView as Window
+    const reactNativeWebView = (window as any).ReactNativeWebView
 
     reactNativeWebView.postMessage(
       JSON.stringify({
         apiHost: ApiHost.getHost(),
         ...message,
-      }),
-      targetOrigin || location.origin,
-      transfer
+      })
     )
   }
 }
