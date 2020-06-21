50a9e293e09f1888463860d57d8f0e389600205e
jhaemin <io@jhaemin.com>
2020-06-21 19:35:01 +0900

    feat: upgrade RequireAuth component

--- src/components/global/RequireAuth/index.tsx
+++ src/components/global/RequireAuth/index.tsx
@@ -1,9 +1,15 @@
 import $ from './style.module.scss'
+import EodiroLink from '@/components/utils/EodiroLink'
 import classNames from 'classnames'
 
-const RequireAuth: React.FC = () => {
+type RequireAuthProps = {
+  className?: string
+}
+
+const RequireAuth: React.FC<RequireAuthProps> = ({ className }) => {
   return (
-    <div className={$['comp-require-auth']}>
+    <div className={classNames($['comp-require-auth'], className)}>
+      <EodiroLink absolute href="/signin" />
       <i className={classNames('f7-icons', $['icon'])}>lock</i>
       <p className={$['paragraph']}>로그인이 필요한 서비스입니다.</p>
     </div>
