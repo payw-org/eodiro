87b2cea03d30c1770ecbcacf7aa119fc77add351
jhaemin <io@jhaemin.com>
2020-06-22 16:53:42 +0900

    chore: change the icon

--- src/components/global/RequireAuth/index.tsx
+++ src/components/global/RequireAuth/index.tsx
@@ -10,7 +10,7 @@ const RequireAuth: React.FC<RequireAuthProps> = ({ className }) => {
   return (
     <div className={classNames($['comp-require-auth'], className)}>
       <EodiroLink absolute href="/signin" />
-      <i className={classNames('f7-icons', $['icon'])}>lock</i>
+      <i className={classNames('f7-icons', $['icon'])}>lock_circle_fill</i>
       <p className={$['paragraph']}>로그인이 필요한 서비스입니다.</p>
     </div>
   )
