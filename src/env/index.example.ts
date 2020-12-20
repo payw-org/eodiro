586b11cc4cc3f132d614f06a8c4e1b8c9fa594bd
Jang Haemin <io@jhaemin.com>
2020-12-20 20:52:40 +0900

    refactor: replace config/* with env/* (#289)

---
+++ src/env/index.example.ts
@@ -0,0 +1,27 @@
+export const env = {
+  // GitHub
+  GITHUB_OAUTH_TOKEN: '',
+
+  // JWT
+  ACCESS_TOKEN_SECRET: '',
+  REFRESH_TOKEN_SECRET: '',
+  ACCESS_TOKEN_LIFETIME: '',
+
+  // Push Notifications
+  PUSH_API_URL: '',
+  TEST_EXPO_PUSH_TOKEN: '',
+
+  // Email
+  MAIL_SERVICE: '',
+  MAIL_HOST: '',
+  MAIL_PORT: 0,
+  MAIL_USERNAME: '',
+  MAIL_PASSWORD: '',
+
+  // CAU Portal
+  CAU_ID: '',
+  CAU_PW: '',
+
+  // Cron
+  TIME_ZONE: '',
+}
