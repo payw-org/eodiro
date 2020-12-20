88212d609d18bc1c757443e0680dfe34812e5844
Jang Haemin <io@jhaemin.com>
2020-12-20 23:25:41 +0900

    refactor: rename the types

--- src/pages/api/auth/join.ts
+++ src/pages/api/auth/join.ts
@@ -1,12 +1,12 @@
 import { nextApi } from '@/modules/next-api-routes-helpers'
 import Auth, { SignUpInfo, SignUpResult } from '@/modules/server/auth'
 
-export type JoinApiRequestBody = SignUpInfo
-export type JoinApiResponseData = SignUpResult
+export type ApiAuthJoinRequestBody = SignUpInfo
+export type ApiAuthJoinResponseData = SignUpResult
 
 export default nextApi({
   post: async ({ req, res }) => {
-    const signUpInfo = req.body as JoinApiRequestBody
+    const signUpInfo = req.body as ApiAuthJoinRequestBody
 
     const signUpResult = await Auth.signUp(signUpInfo)
 
