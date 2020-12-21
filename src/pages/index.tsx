7964f9a3502ee8971f139d5c8aa8ca738caf7a8c
Jang Haemin <io@jhaemin.com>
2020-12-21 22:26:11 +0900

    chore: remove test codes and return directly

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,6 @@
 import Home from '@/components/home/Home'
-import EodiroHttpCookie from '@/modules/eodiro-http-cookie'
 import { NextPage } from 'next'
-import { useEffect } from 'react'
 
-const Page: NextPage = () => {
-  useEffect(() => {
-    EodiroHttpCookie.set({
-      expires: new Date('2020-12-31').toUTCString(),
-      name: 'test_cookie',
-      value: 'what',
-    }).then((res) => {
-      console.log(res)
-    })
-  }, [])
-
-  return <Home />
-}
+const Page: NextPage = () => <Home />
 
 export default Page
