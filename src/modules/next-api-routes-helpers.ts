fde6bb814252ef31ba4281d9caa1992bb66404f4
Jang Haemin <io@jhaemin.com>
2020-12-21 22:27:08 +0900

    feat(next api): add validation functions

--- src/modules/next-api-routes-helpers.ts
+++ src/modules/next-api-routes-helpers.ts
@@ -13,12 +13,78 @@ export const nextApi = (handler: {
 
   switch (method) {
     case 'GET':
-      await handler.get({ req, res })
+      if (handler.get) {
+        await handler.get({ req, res })
+      }
       break
     case 'POST':
-      await handler.post({ req, res })
+      if (handler.post) {
+        await handler.post({ req, res })
+      }
       break
     default:
       break
   }
+}
+
+type DataType =
+  | 'string'
+  | 'number'
+  | 'bigint'
+  | 'boolean'
+  | 'symbol'
+  | 'undefined'
+  | 'object'
+  | 'function'
+
+export function validateRequiredBody(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  requiredBodyStructure: Record<string, DataType>
+) {
+  const actualBody = req.body
+
+  for (const key in requiredBodyStructure) {
+    if (Object.prototype.hasOwnProperty.call(requiredBodyStructure, key)) {
+      const dataType = requiredBodyStructure[key]
+      const bodyValue = actualBody[key]
+
+      if (
+        bodyValue === undefined ||
+        bodyValue === null ||
+        typeof actualBody[key] !== dataType
+      ) {
+        const message = `Field ${key} is wrong`
+        res.json({ error: { message } })
+
+        return false
+      }
+    }
+  }
+
+  return true
+}
+
+export const extractToken = (
+  req: NextApiRequest,
+  res: NextApiResponse
+): null | string => {
+  const { authorization } = req.headers
+
+  if (!authorization || typeof authorization !== 'string') {
+    res.status(401).end()
+    return null
+  }
+
+  const splittedAuth = authorization.split(' ')
+
+  const bearerPart = splittedAuth[0]
+  const token = splittedAuth[1]
+
+  if (bearerPart !== 'Bearer' || !token) {
+    res.status(401).end()
+    return null
+  }
+
+  return token
 }
