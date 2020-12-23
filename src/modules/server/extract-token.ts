cef863938f81e74d867a79193dfa2434e77b0eb8
Jang Haemin <io@jhaemin.com>
2020-12-24 01:46:53 +0900

    refactor: improve the logic by reducing indents

--- src/modules/server/extract-token.ts
+++ src/modules/server/extract-token.ts
@@ -1,42 +1,39 @@
 import { constants } from '@/constants'
-import { getCookies } from '@/pages/api/cookies'
+import { getCookie } from '@/pages/api/cookie'
+import { IncomingMessage, ServerResponse } from 'http'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export const extractToken = (
-  req: NextApiRequest,
-  res: NextApiResponse,
+  req: NextApiRequest | IncomingMessage,
+  res: NextApiResponse | ServerResponse,
   type: 'access' | 'refresh'
 ): null | string => {
+  const cookie = getCookie(req)
+  const tokenFromCookie =
+    cookie[
+      type === 'access'
+        ? constants.EDR_ACCESS_TOKEN_NAME
+        : constants.EDR_REFRESH_TOKEN_NAME
+    ]
+
+  if (tokenFromCookie) {
+    return tokenFromCookie
+  }
+
   const { authorization } = req.headers
 
   if (!authorization || typeof authorization !== 'string') {
-    // res.status(401).end()
-    // return null
-    const cookies = getCookies(req)
-    const token =
-      cookies[
-        type === 'access'
-          ? constants.EDR_ACCESS_TOKEN_NAME
-          : constants.EDR_REFRESH_TOKEN_NAME
-      ]
-
-    if (!token) {
-      res.status(401).end()
-      return null
-    }
-  } else {
-    const splittedAuth = authorization.split(' ')
-
-    const bearerPart = splittedAuth[0]
-    const token = splittedAuth[1]
-
-    if (bearerPart !== 'Bearer' || !token) {
-      res.status(401).end()
-      return null
-    }
-
-    return token
+    return null
   }
 
-  return null
+  const splittedAuth = authorization.split(' ')
+
+  const bearerPart = splittedAuth[0]
+  const tokenFromHeaders = splittedAuth[1]
+
+  if (bearerPart !== 'Bearer' || !tokenFromHeaders) {
+    return null
+  }
+
+  return tokenFromHeaders
 }
