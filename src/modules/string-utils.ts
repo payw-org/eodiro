c769c8c7e599fde0311b4b883adfe59873c4113d
Jang Haemin <io@jhaemin.com>
2020-12-28 01:29:07 +0900

    fix: use arrow function instead of unnamed one

--- src/modules/string-utils.ts
+++ src/modules/string-utils.ts
@@ -1,6 +1,5 @@
-export const camelToKebab = function (str: string) {
-  return str
+export const camelToKebab = (str: string) =>
+  str
     .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
     .replace(/([A-Z])([A-Z])(?=[a-z])/g, '$1-$2')
     .toLowerCase()
-}
