df39990c41f991687860561815a24784bb3deec4
Jang Haemin <io@jhaemin.com>
2020-12-27 19:20:48 +0900

    feat: handle refresh get request

--- src/pages/api/auth/refresh.ts
+++ src/pages/api/auth/refresh.ts
@@ -1,22 +1,80 @@
-import { AuthData, signAccessToken, verifyToken } from '@/modules/jwt'
-import { nextApi } from '@/modules/next-api-routes-helpers'
+import { eodiroConsts } from '@/constants'
+import {
+  AuthData,
+  JwtError,
+  JwtErrorName,
+  signAccessToken,
+  verifyToken,
+} from '@/modules/jwt'
+import { createHandler, nextApi } from '@/modules/next-api-routes-helpers'
 import { extractToken } from '@/modules/server/extract-token'
+import { redirect } from '@/modules/server/redirect'
+import { getCookie, setCookie } from '../cookie'
+
+export type ApiAuthRefreshResData = {
+  accessToken: string
+}
 
 export default nextApi({
-  post: async (req, res) => {
-    const refreshToken = extractToken(req, res, 'refresh')
+  post: createHandler<ApiAuthRefreshResData | { error: JwtError }>(
+    async (req, res) => {
+      const refreshToken = extractToken(req, res, 'refresh')
 
-    if (!refreshToken) return
+      if (!refreshToken) {
+        res.status(401).end()
 
-    const [err, authData] = await verifyToken(refreshToken, 'refresh')
+        return
+      }
 
-    if (err) {
-      res.status(401).json({ error: err })
-    } else {
-      // Sign new access token
-      const newAccessToken = signAccessToken(authData as AuthData)
+      const [err, authData] = await verifyToken(refreshToken, 'refresh')
 
-      res.json({ accessToken: newAccessToken })
+      if (err) {
+        res.status(401).json({ error: err })
+      } else {
+        // Sign new access token
+        const newAccessToken = signAccessToken(authData as AuthData)
+
+        // Set cookie
+        setCookie(req, res, {
+          name: eodiroConsts.EDR_ACCESS_TOKEN_NAME,
+          value: newAccessToken,
+          expires: new Date('2038-01-01').toUTCString(),
+        })
+
+        res.json({ accessToken: newAccessToken })
+      }
     }
+  ),
+  get: async (req, res) => {
+    const accessToken = extractToken(req, res, 'access')
+    const [accessErr] = await verifyToken(accessToken, 'access')
+
+    if (accessErr && accessErr.name === JwtErrorName.TokenExpiredError) {
+      // Access token has expired
+
+      const refreshToken = extractToken(req, res, 'refresh')
+      const [refreshErr, authData] = await verifyToken(refreshToken, 'refresh')
+
+      if (refreshErr) {
+        // Refresh token error
+      } else {
+        // Sign new access token
+        const newAccessToken = signAccessToken(authData as AuthData)
+
+        // Set access token to cookie
+        setCookie(req, res, {
+          name: eodiroConsts.EDR_ACCESS_TOKEN_NAME,
+          value: newAccessToken,
+          expires: new Date('2038-01-01').toUTCString(),
+        })
+
+        const lastPath = getCookie(req, eodiroConsts.LAST_PATH)
+        redirect(res, lastPath)
+
+        return
+      }
+    }
+
+    redirect(res, '/login-please')
   },
 })
