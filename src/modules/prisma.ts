482548d09b79ef5a582f0c9b182ac11b29dfa652
Jang Haemin <io@jhaemin.com>
2020-12-30 21:06:37 +0900

    fix: prevent infinite instance regeneration in dev mode

--- src/modules/prisma.ts
+++ src/modules/prisma.ts
@@ -1,7 +1,20 @@
 import { PrismaClient } from '@prisma/client'
 import { prismaTimeMode } from './time'
 
-export const prisma = new PrismaClient()
+// eslint-disable-next-line import/no-mutable-exports
+let prisma: PrismaClient
+
+// Fix endless prisma client re-initialization on every HMR
+// Related issue https://github.com/prisma/prisma/issues/1983#issuecomment-620621213
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!(global as any).prisma) {
+    ;(global as any).prisma = new PrismaClient()
+  }
+
+  prisma = (global as any).prisma
+}
 
 if (prisma.$use) {
   prisma.$use(async (params, next) => {
@@ -11,3 +24,5 @@ if (prisma.$use) {
     return prismaTimeMode(result)
   })
 }
+
+export { prisma }
