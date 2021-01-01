05a801de303a0c1b5c84e024edaf9494da68601a
Jang Haemin <io@jhaemin.com>
2021-01-01 19:41:34 +0900

    fix: fix duplicate middlewares

--- src/modules/prisma.ts
+++ src/modules/prisma.ts
@@ -1,28 +1,32 @@
 import { PrismaClient } from '@prisma/client'
-import { prismaTimeMode } from './time'
+import { prismaTimeMod } from './time'
 
 // eslint-disable-next-line import/no-mutable-exports
 let prisma: PrismaClient
+
+function timeMod(prismaInstance: PrismaClient) {
+  if (prismaInstance.$use) {
+    prismaInstance.$use(async (params, next) => {
+      const result = await next(params)
+
+      // Subtract 9 hours from UTC
+      return prismaTimeMod(result)
+    })
+  }
+}
 
 // Fix endless prisma client re-initialization on every HMR
 // Related issue https://github.com/prisma/prisma/issues/1983#issuecomment-620621213
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
+  timeMod(prisma)
 } else {
   if (!(global as any).prisma) {
     ;(global as any).prisma = new PrismaClient()
+    timeMod((global as any).prisma)
   }
 
   prisma = (global as any).prisma
-}
-
-if (prisma.$use) {
-  prisma.$use(async (params, next) => {
-    const result = await next(params)
-
-    // Subtract 9 hours from UTC
-    return prismaTimeMode(result)
-  })
 }
 
 export { prisma }
