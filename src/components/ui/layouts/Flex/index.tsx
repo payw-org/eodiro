e42c2e136fee67bfe62935d4767c73cbfffa3a7e
Jang Haemin <io@jhaemin.com>
2021-01-02 11:33:14 +0900

    feat: add align-items, justify-content props

--- src/components/ui/layouts/Flex/index.tsx
+++ src/components/ui/layouts/Flex/index.tsx
@@ -9,6 +9,15 @@ type FlexProps = {
   center?: boolean
   flex?: number
   minWidth?: string
+  alignCenter?: boolean
+  alignStart?: boolean
+  alignEnd?: boolean
+  justifyCenter?: boolean
+  justifyStart?: boolean
+  justifyEnd?: boolean
+  justifyBetween?: boolean
+  wrap?: boolean
+  onClick?: () => void
 }
 
 export function Flex({
@@ -19,23 +28,51 @@ export function Flex({
   center,
   flex,
   minWidth,
+  alignCenter,
+  alignStart,
+  alignEnd,
+  justifyCenter,
+  justifyStart,
+  justifyEnd,
+  justifyBetween,
+  wrap,
+  onClick,
 }: FlexProps) {
+  const builtClassName = classNames(
+    $['center'],
+    {
+      [$['row']]: row,
+      [$['column']]: column,
+      [$['center']]: center,
+      [$['align-center']]: alignCenter,
+      [$['align-start']]: alignStart,
+      [$['align-end']]: alignEnd,
+      [$['justify-center']]: justifyCenter,
+      [$['justify-start']]: justifyStart,
+      [$['justify-end']]: justifyEnd,
+      [$['justify-between']]: justifyBetween,
+      [$['wrap']]: wrap,
+    },
+    className
+  )
+
+  const style: React.CSSProperties = { flex, minWidth }
+
+  if (onClick) {
+    return (
+      <button
+        type="button"
+        className={builtClassName}
+        style={style}
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    )
+  }
+
   return (
-    <div
-      className={classNames(
-        $['center'],
-        {
-          [$['row']]: row,
-          [$['column']]: column,
-          [$['center']]: center,
-        },
-        className
-      )}
-      style={{
-        flex,
-        minWidth,
-      }}
-    >
+    <div className={builtClassName} style={style}>
       {children}
     </div>
   )
