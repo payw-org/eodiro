5bcc1d09ad1fcc05091b17f5f5c4b59cdf119dd1
Jang Haemin <io@jhaemin.com>
2021-01-03 16:25:11 +0900

    chore: reorder the crons

--- src/cron/crontab.sh
+++ src/cron/crontab.sh
@@ -6,13 +6,13 @@
 # > sudo service cron start
 
 # update random nicknames at 00:00
-0 0 * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/web/build/cron/update-random-nicknames
+0 0 * * * /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/web/build/cron/update-random-nicknames
 
 # check notice every 15 minutes
-*/15 * * * *NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/web/build/cron/check-notice
+*/15 * * * * /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/web/build/cron/check-notice
+
+# clear pending users every 30 minutes
+*/30 * * * * /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/web/build/cron/clear-pending-users
 
 # seed cafeteria menus at 03:00 everyday
 # 0 3 * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/web/build/cron/seed-cafeteria-menus
-
-# clear pending users every 30 minutes
-*/30 * * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/web/build/cron/clear-pending-users
