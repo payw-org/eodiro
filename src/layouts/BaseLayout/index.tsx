4211589f221a9dde5fd538a6a1521908566756ea
Jang Haemin <io@jhaemin.com>
2021-01-05 20:34:59 +0900

    fix: init inside recoil root

--- src/layouts/BaseLayout/index.tsx
+++ src/layouts/BaseLayout/index.tsx
@@ -1,10 +1,42 @@
+import { authState } from '@/atoms/auth'
 import GlobalFooter from '@/components/global/GlobalFooter'
 import Navigation, { NavContextProvider } from '@/components/global/Navigation'
 import { isInApp } from '@/modules/booleans/is-in-app'
-import React from 'react'
+import { eodiroRequest, registerPush } from '@/modules/eodiro-request'
+import { AuthData } from '@/modules/jwt'
+import { ApiAuthVerifyResData } from '@/pages/api/auth/verify'
+import React, { useEffect } from 'react'
+import { useSetRecoilState } from 'recoil'
 import $ from './style.module.scss'
 
-const BaseLayout: React.FC = ({ children }) => {
+const BaseLayout: React.FC<{
+  shouldCheckAuth: boolean
+}> = ({ children, shouldCheckAuth }) => {
+  const setAuthData = useSetRecoilState(authState)
+
+  useEffect(() => {
+    async function init() {
+      try {
+        const responseData = await eodiroRequest<null, ApiAuthVerifyResData>({
+          url: '/api/auth/verify',
+          method: 'POST',
+        })
+
+        await registerPush()
+
+        setAuthData(responseData.authData as AuthData)
+      } catch (error) {
+        // JWT verification error
+        window.alert(error)
+      }
+    }
+
+    if (shouldCheckAuth) {
+      init()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <NavContextProvider>
       <div id={$['eodiro-app-scaffold']}>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import '@/assets/styles/global/globalstyle.scss'
-import { authState } from '@/atoms/auth'
 import PageInfo from '@/components/utils/PageInfo'
 import { eodiroConsts } from '@/constants'
 import { phrases } from '@/constants/phrases'
 import BaseLayout from '@/layouts/BaseLayout'
-import { eodiroRequest, registerPush } from '@/modules/eodiro-request'
-import { AuthData } from '@/modules/jwt'
+import { eodiroRequest } from '@/modules/eodiro-request'
 import { reactNativeWebViewPostMessage } from '@/modules/native/react-native-webview'
 import { isDev } from '@/modules/utils/is-dev'
 import 'intersection-observer'
@@ -14,10 +12,9 @@ import { AppContextType } from 'next/dist/next-server/lib/utils'
 import Head from 'next/head'
 import Router from 'next/router'
 import React, { useEffect } from 'react'
-import { RecoilRoot, useSetRecoilState } from 'recoil'
+import { RecoilRoot } from 'recoil'
 import 'swiper/swiper.scss'
 import { SWRConfig } from 'swr'
-import { ApiAuthVerifyResData } from './api/auth/verify'
 import { getCookie } from './api/cookie'
 import './_document.scss'
 
@@ -91,31 +88,6 @@ export default function EdrApp({
     })
   }, [])
 
-  const setAuthData = useSetRecoilState(authState)
-
-  useEffect(() => {
-    async function init() {
-      try {
-        const responseData = await eodiroRequest<null, ApiAuthVerifyResData>({
-          url: '/api/auth/verify',
-          method: 'POST',
-        })
-
-        await registerPush()
-
-        setAuthData(responseData.authData as AuthData)
-      } catch (error) {
-        // JWT verification error
-        window.alert(error)
-      }
-    }
-
-    if (shouldCheckAuth) {
-      init()
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
   return (
     <>
       <Head>
@@ -164,7 +136,7 @@ export default function EdrApp({
             },
           }}
         >
-          <BaseLayout>
+          <BaseLayout shouldCheckAuth={shouldCheckAuth}>
             <Component {...pageProps} />
           </BaseLayout>
         </SWRConfig>
