d41c68d5c12279b07c4f802521375fbdb3edf999
Jang Haemin <io@jhaemin.com>
2021-01-08 22:33:15 +0900

    feat: human-readable status codes

---
+++ src/constants/http-status-code.ts
@@ -0,0 +1,14 @@
+// Reference
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+
+// Successfull
+export const OK = 200
+
+// Client errors
+export const BAD_REQUEST = 400
+export const UNAUTHORIZED = 401
+export const FORBIDDEN = 403
+export const NOT_FOUND = 404
+
+// Server errors
+export const INTERNAL_SERVER_ERROR = 500
