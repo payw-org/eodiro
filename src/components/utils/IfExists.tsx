dc55d9e33e030a4fb4ac3cbe16c38344c1f182a6
Jang Haemin <io@jhaemin.com>
2021-01-11 22:31:51 +0900

    fix: server error only when error exists
    * Replace default server error (wrong) with the spinner

--- src/components/utils/IfExists.tsx
+++ src/components/utils/IfExists.tsx
@@ -1,19 +1,24 @@
 import { UnauthorizedError } from '@/modules/eodiro-request'
 import Information from '../global/Information'
 import ServerError from '../global/ServerError'
+import { Spinner } from '../global/Spinner'
 
 export const IfExists: React.FC<{ error: any; data: any }> = ({
   children,
   error,
   data,
 }) => {
+  if (error) {
+    if (error?.name === UnauthorizedError.Unauthorized) {
+      return <Information title="로그인하세요." />
+    }
+
+    return <ServerError />
+  }
+
   if (data) {
     return <>{children}</>
   }
 
-  if (error?.name === UnauthorizedError.Unauthorized) {
-    return <Information title="로그인하세요." />
-  }
-
-  return <ServerError />
+  return <Spinner />
 }
