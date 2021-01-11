35a06fe725b2396c2ab5475cf961ce02d26cc12f
Jang Haemin <io@jhaemin.com>
2021-01-11 22:33:39 +0900

    chore: self-close empty divs

--- src/components/global/Spinner/index.tsx
+++ src/components/global/Spinner/index.tsx
@@ -3,18 +3,18 @@ import $ from './style.module.scss'
 export const Spinner: React.FC = () => {
   return (
     <div className={$['lds-spinner']}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
     </div>
   )
 }
