800f3004c57a4a4f52a0b10e27a9473d3a34e250
Jang Haemin <io@jhaemin.com>
2021-01-13 22:35:20 +0900

    feat: create pin boards related apis (#306)

---
+++ src/pages/api/community/get-pinned-boards.ts
@@ -0,0 +1,38 @@
+import { createHandler, nextApi } from '@/modules/next-api-routes-helpers'
+import { prisma } from '@/modules/prisma'
+import { requireAuthMiddleware } from '@/modules/server/middlewares/require-auth'
+import { SafeCommunityBoard } from '@/types/schema'
+
+export type ApiGetPinnedBoardsResData = SafeCommunityBoard[]
+
+export async function getPinnedBoards(data: { userId: number }) {
+  const { userId } = data
+
+  const pinnedBoards = await prisma.communityBoard.findMany({
+    where: {
+      isDeleted: false,
+      communityBoardPins: {
+        some: { userId },
+      },
+    },
+    orderBy: [{ priority: 'desc' }, { name: 'desc' }],
+    select: {
+      id: true,
+      name: true,
+    },
+  })
+
+  return pinnedBoards
+}
+
+export default nextApi({
+  get: createHandler<ApiGetPinnedBoardsResData>(async (req, res) => {
+    await requireAuthMiddleware(req, res)
+
+    const { user } = req
+
+    const pinnedBoards = await getPinnedBoards({ userId: user.id })
+
+    return res.json(pinnedBoards)
+  }),
+})
---
+++ src/pages/api/community/get-recent-boards.ts
@@ -0,0 +1,40 @@
+import { createHandler, nextApi } from '@/modules/next-api-routes-helpers'
+import { prisma } from '@/modules/prisma'
+import { requireAuthMiddleware } from '@/modules/server/middlewares/require-auth'
+import { dbTime } from '@/modules/time'
+import { SafeCommunityBoard } from '@/types/schema'
+import dayjs from 'dayjs'
+import { getPinnedBoards } from './get-pinned-boards'
+
+export type ApiGetRecentBoardsResData = SafeCommunityBoard[]
+
+export default nextApi({
+  get: createHandler<ApiGetRecentBoardsResData>(async (req, res) => {
+    await requireAuthMiddleware(req, res)
+
+    const { user } = req
+
+    const recentBoards = await prisma.communityBoard.findMany({
+      where: {
+        activeAt: {
+          gte: dbTime(dayjs().subtract(3, 'day').toDate()),
+        },
+      },
+      orderBy: [{ priority: 'desc' }, { activeAt: 'desc' }],
+      select: {
+        id: true,
+        name: true,
+      },
+    })
+
+    const pinnedBoards = await getPinnedBoards({ userId: user.id })
+
+    const filteredRecentBoards = recentBoards.filter(
+      (board) =>
+        pinnedBoards.findIndex((pinnedBoard) => pinnedBoard.id === board.id) ===
+        -1
+    )
+
+    return res.json(filteredRecentBoards)
+  }),
+})
