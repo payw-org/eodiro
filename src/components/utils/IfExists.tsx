663241ee88608bd537f711359a0a2d3b4ca41b66
Jang Haemin <io@jhaemin.com>
2021-01-13 21:27:37 +0900

    feat(dev): render conditionally

--- src/components/utils/IfExists.tsx
+++ src/components/utils/IfExists.tsx
@@ -1,13 +1,17 @@
 import { UnauthorizedError } from '@/modules/eodiro-request'
+import React from 'react'
 import Information from '../global/Information'
 import ServerError from '../global/ServerError'
 import { Spinner } from '../global/Spinner'
+import { Flex } from '../ui/layouts/Flex'
 
-export const IfExists: React.FC<{ error: any; data: any }> = ({
-  children,
-  error,
-  data,
-}) => {
+export const IfExists: React.FC<{
+  error: any
+  data: any
+  condition?: boolean
+}> = ({ children, error, data, condition }) => {
+  const childrenArray = React.Children.toArray(children)
+
   if (error) {
     if (error?.name === UnauthorizedError.Unauthorized) {
       return <Information title="로그인하세요." />
@@ -17,8 +21,20 @@ export const IfExists: React.FC<{ error: any; data: any }> = ({
   }
 
   if (data) {
-    return <>{children}</>
+    if (condition === undefined) {
+      return <>{children}</>
+    }
+    if (condition) {
+      return <>{childrenArray[0]}</>
+    }
+    if (childrenArray[1]) {
+      return <>{childrenArray[1]}</>
+    }
   }
 
-  return <Spinner />
+  return (
+    <Flex center>
+      <Spinner />
+    </Flex>
+  )
 }
