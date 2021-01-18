0accfbabc4efcf6c3f79275509d4e5c8e09b6dcc
Jang Haemin <io@jhaemin.com>
2021-01-18 11:34:34 +0900

    refactor: destructure the object

--- src/components/community/PostsList/index.tsx
+++ src/components/community/PostsList/index.tsx
@@ -2,50 +2,60 @@ import { Icon } from '@/components/ui/Icon'
 import { Flex } from '@/components/ui/layouts/Flex'
 import { friendlyTime } from '@/modules/time'
 import { CommunityPostWithCounts } from '@/pages/api/types'
+import { SafeCommunityPost } from '@/types/schema'
 import { communityPostPageUrl } from '@/utils/page-urls'
 import classNames from 'classnames'
 import Link from 'next/link'
 import $ from './style.module.scss'
 
 const PostItem: React.FC<{
-  post: CommunityPostWithCounts
+  post: SafeCommunityPost
 }> = ({ post }) => {
+  const {
+    title,
+    boardId,
+    likesCount,
+    bookmarksCount,
+    commentsCount,
+    postedAt,
+  } = post
+
   return (
-    <Link href={communityPostPageUrl(post.boardId, post.id)}>
+    <Link href={communityPostPageUrl(boardId, post.id)}>
       <div key={post.id} className={$['post-item']}>
         <Flex column alignStart>
-          <p className={$['post-title']}>{post.title}</p>
+          <p className={$['post-title']}>{title}</p>
           <Flex className={$['counts']} alignCenter>
             <Flex
               alignCenter
               className={classNames($['count-item'], $['likes-count'], {
-                [$['empty']]: post.communityPostLikesCount === 0,
+                [$['empty']]: likesCount === 0,
               })}
             >
               <Icon name="hand_thumbsup" />
-              {post.communityPostLikesCount}
+              {likesCount}
             </Flex>
             <Flex
               alignCenter
               className={classNames($['count-item'], $['bookmarks-count'], {
-                [$['empty']]: post.communityPostBookmarksCount === 0,
+                [$['empty']]: bookmarksCount === 0,
               })}
             >
               <Icon name="bookmark" />
-              {post.communityPostBookmarksCount}
+              {bookmarksCount}
             </Flex>
             <Flex
               alignCenter
               className={classNames($['count-item'], $['comments-count'], {
-                [$['empty']]: post.communityCommentsCount === 0,
+                [$['empty']]: commentsCount === 0,
               })}
             >
               <Icon name="bubble_left" />
-              {post.communityCommentsCount}
+              {commentsCount}
             </Flex>
           </Flex>
         </Flex>
-        <span className={$['post-time']}>{friendlyTime(post.postedAt)}</span>
+        <span className={$['post-time']}>{friendlyTime(postedAt)}</span>
       </div>
     </Link>
   )
