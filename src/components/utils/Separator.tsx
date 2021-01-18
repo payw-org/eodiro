5aaa9adc53ab1c1bfb0a8901fa4361f05110784c
Jang Haemin <io@jhaemin.com>
2021-01-18 11:36:06 +0900

    doc: deprecate the Separator component

--- src/components/utils/Separator.tsx
+++ src/components/utils/Separator.tsx
@@ -1,6 +1,9 @@
 import $$ from 'classnames'
 import $ from './Separator.module.scss'
 
+/**
+ * @deprecated Use `<hr />` instead.
+ */
 export default function Separator({ className }: { className?: string }) {
   return <div className={$$($['separator'], className)} />
 }
