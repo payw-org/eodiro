56bb8e1d7b9c23e044b88d3dc7cf9c2799609027
Jang Haemin <io@jhaemin.com>
2021-02-03 19:51:06 +0900

    refactor: fetch data from client side (#335)

--- src/components/open-source/OpenSourcePage/index.tsx
+++ src/components/open-source/OpenSourcePage/index.tsx
@@ -1,13 +1,111 @@
+import { Spinner } from '@/components/global/Spinner'
+import { env } from '@/env'
 import Body from '@/layouts/BaseLayout/Body'
 import { Contributor } from '@/types/github-api'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { Else, If, Then } from 'react-if'
 import PageInfo from '../../utils/PageInfo'
 import $ from './style.module.scss'
 
-export type OpenSourcePageProps = {
-  contributors: Contributor[]
-}
+const OpenSourcePage: React.FC = () => {
+  const [contributors, setContributors] = useState<Contributor[] | null>(null)
 
-const OpenSourcePage: React.FC<OpenSourcePageProps> = ({ contributors }) => {
+  useEffect(() => {
+    async function getContributors() {
+      const { data: eodiroContributors } = await axios.get<Contributor[]>(
+        'https://api.github.com/repos/payw-org/eodiro/contributors',
+        {
+          auth: {
+            username: 'jhaemin',
+            password: env.GITHUB_OAUTH_TOKEN,
+          },
+        }
+      )
+      const { data: nextContributors } = await axios.get<Contributor[]>(
+        'https://api.github.com/repos/payw-org/eodiro-next/contributors',
+        {
+          auth: {
+            username: 'jhaemin',
+            password: env.GITHUB_OAUTH_TOKEN,
+          },
+        }
+      )
+      const { data: api1Contributors } = await axios.get<Contributor[]>(
+        'https://api.github.com/repos/payw-org/eodiro-api/contributors',
+        {
+          auth: {
+            username: 'jhaemin',
+            password: env.GITHUB_OAUTH_TOKEN,
+          },
+        }
+      )
+      const { data: api2Contributors } = await axios.get<Contributor[]>(
+        'https://api.github.com/repos/payw-org/eodiro-server/contributors',
+        {
+          auth: {
+            username: 'jhaemin',
+            password: env.GITHUB_OAUTH_TOKEN,
+          },
+        }
+      )
+
+      const rawContributors = eodiroContributors || []
+
+      if (nextContributors) {
+        for (const nextUser of nextContributors) {
+          const index = rawContributors.findIndex(
+            (user) => user.login === nextUser.login
+          )
+
+          if (index === -1) {
+            rawContributors.push(nextUser)
+          } else {
+            rawContributors[index].contributions += nextUser.contributions
+          }
+        }
+      }
+
+      if (api1Contributors) {
+        for (const api1User of api1Contributors) {
+          const index = rawContributors.findIndex(
+            (user) => user.login === api1User.login
+          )
+
+          if (index === -1) {
+            rawContributors.push(api1User)
+          } else {
+            rawContributors[index].contributions += api1User.contributions
+          }
+        }
+      }
+
+      if (api2Contributors) {
+        for (const api2User of api2Contributors) {
+          const index = rawContributors.findIndex(
+            (user) => user.login === api2User.login
+          )
+
+          if (index === -1) {
+            rawContributors.push(api2User)
+          } else {
+            rawContributors[index].contributions += api2User.contributions
+          }
+        }
+      }
+
+      const filteredContributors = rawContributors.filter(
+        (contributor) => !contributor.login.startsWith('dependabot')
+      )
+
+      filteredContributors.sort((a, b) => b.contributions - a.contributions)
+
+      setContributors(filteredContributors)
+    }
+
+    getContributors()
+  }, [])
+
   return (
     <Body pageTitle="오픈소스" bodyClassName={$['component-open-source']}>
       <PageInfo
@@ -44,26 +142,39 @@ const OpenSourcePage: React.FC<OpenSourcePageProps> = ({ contributors }) => {
           개발자 이름 하단에 표시되는 숫자는 총 기여(contribution) 횟수입니다.
         </p>
 
-        <div className={$['users']}>
-          {contributors.map((user) => (
-            <a
-              href={user.html_url}
-              key={user.id}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={$['user-link']}
-            >
-              <div className={$['user']}>
-                <img
-                  src={user.avatar_url}
-                  alt="Avatar"
-                  className={$['avatar']}
-                />
-                <h3 className={$['name']}>{user.login}</h3>
-                <span className={$['contributions']}>{user.contributions}</span>
+        <div className="mt-8">
+          <If condition={!!contributors}>
+            <Then>
+              <div className={$['users']}>
+                {contributors?.map((user) => (
+                  <a
+                    href={user.html_url}
+                    key={user.id}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={$['user-link']}
+                  >
+                    <div className={$['user']}>
+                      <img
+                        src={user.avatar_url}
+                        alt="Avatar"
+                        className={$['avatar']}
+                      />
+                      <h3 className={$['name']}>{user.login}</h3>
+                      <span className={$['contributions']}>
+                        {user.contributions}
+                      </span>
+                    </div>
+                  </a>
+                ))}
               </div>
-            </a>
-          ))}
+            </Then>
+            <Else>
+              <div className="flex justify-center w-full">
+                <Spinner />
+              </div>
+            </Else>
+          </If>
         </div>
       </section>
     </Body>
