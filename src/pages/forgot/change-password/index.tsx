600db3058a1a6501ff6f63798ea76cf9862a7425
Jang Haemin <io@jhaemin.com>
2021-03-20 10:40:58 +0900

    Change password (#338)

--- src/pages/forgot/change-password/index.tsx
+++ src/pages/forgot/change-password/index.tsx
@@ -1,82 +1,110 @@
 import { Button, LineInput } from '@/components/ui'
+import { BAD_REQUEST, NOT_FOUND } from '@/constants/http-status-code'
 import Body from '@/layouts/BaseLayout/Body'
+import ApiHost from '@/modules/api-host'
 import EodiroDialog from '@/modules/client/eodiro-dialog'
-import { logInUrl } from '@/utils/page-urls'
-import { ApiAuthJoinRequestBody } from '@payw/eodiro-server-types/api/auth/join'
-import Axios from 'axios'
-import { NextPage } from 'next'
+import { eodiroRequest } from '@/modules/eodiro-request'
+import { ApiAuthChangePasswordReqBody } from '@payw/eodiro-server-types/api/auth/change-password'
+import {
+  ApiAuthValidateRequestBody,
+  ApiAuthValidateResponseData,
+} from '@payw/eodiro-server-types/api/auth/validate'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 import $ from './style.module.scss'
 
-type ChangePasswordRequestPageProps = {
-  userId: number
-  valid: boolean
-}
-
-const ChangePasswordRequestPage: NextPage<ChangePasswordRequestPageProps> = ({
-  userId,
-  valid,
-}) => {
+const ChangePasswordRequestPage = () => {
   const router = useRouter()
+  const token = router.query.t as string
   const [password, setPassword] = useState('')
-  const [validating, setValidating] = useState(false)
+  const [isValidating, setIsValidating] = useState(false)
+  const [isAvailable, setIsAvailable] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+  const buttonDisabled = !isAvailable || isValidating
 
-  async function change(): Promise<void> {
-    setValidating(true)
+  function validatePassword(newPassword: string) {
+    setIsValidating(true)
 
-    const { data } = await Axios.post('/api/auth/validate', {
-      password,
-    } as ApiAuthJoinRequestBody)
+    eodiroRequest<ApiAuthValidateRequestBody, ApiAuthValidateResponseData>({
+      method: 'POST',
+      url: ApiHost.resolve('/auth/validate'),
+      data: {
+        password: newPassword,
+      },
+    })
+      .then((data) => {
+        if (data.password?.isValid) {
+          setErrorMessage('')
+          setIsAvailable(true)
+        } else {
+          setErrorMessage(data.password?.error?.message as string)
+          setIsAvailable(false)
+        }
+      })
+      .catch((error) => {
+        console.error(error)
+        setIsAvailable(false)
+      })
+      .finally(() => {
+        setIsValidating(false)
+      })
+  }
 
-    if (!data.password?.isValid) {
-      new EodiroDialog().alert(data.password?.error?.message)
-    } else {
-      try {
-        await Axios.post('/api/auth/change-password', {
-          userId,
-          password,
-        })
+  function changePassword(newPassword: string) {
+    eodiroRequest<ApiAuthChangePasswordReqBody>({
+      method: 'POST',
+      url: ApiHost.resolve('/auth/change-password'),
+      data: {
+        newPassword,
+        token: token ?? '',
+      },
+    })
+      .then(() => {
+        new EodiroDialog().alert('암호가 변경되었습니다.')
+        router.push('/log-in')
+      })
+      .catch((error) => {
+        const status = error.response?.status
 
-        new EodiroDialog().alert('변경되었습니다. 다시 로그인 해주세요.')
-        router.push(logInUrl)
-      } catch (error) {
-        new EodiroDialog().alert(
-          '암호 변경에 실패했습니다. 반복 시 문의해주세요.'
-        )
-      }
-    }
+        if (status === NOT_FOUND) {
+          new EodiroDialog().alert(
+            '암호 변경 요청이 만료되었거나 유효하지 않습니다. 다시 요청해주세요.'
+          )
+          router.push('/log-in')
+        } else if (status === BAD_REQUEST) {
+          new EodiroDialog().alert('암호 조건을 다시 확인해주세요.')
+        }
 
-    setValidating(false)
+        console.error(error)
+      })
   }
 
   return (
     <Body
-      pageTitle={valid ? '새 암호 입력' : '오류'}
+      pageTitle="암호 변경"
       titleAlign="center"
       centered
       bodyClassName={$['change-password-request']}
     >
-      {valid && (
-        <>
-          <LineInput
-            type="password"
-            value={password}
-            setValue={setPassword}
-            alignCenter
-            onEnter={change}
-            disabled={validating}
-          />
-          <Button
-            label="변경하기"
-            full
-            className={$['change-btn']}
-            disabled={validating}
-            onClick={change}
-          />
-        </>
+      <LineInput
+        type="password"
+        value={password}
+        placeholder="새 암호를 입력하세요."
+        setValue={setPassword}
+        alignCenter
+        onChangeThrottle={[validatePassword]}
+        disabled={isValidating}
+      />
+      {errorMessage && (
+        <span className="text-eodiro-primary-color mt-2">{errorMessage}</span>
       )}
-      {!valid && <p>암호 변경 요청이 만료되었거나 유효하지 않습니다.</p>}
+      <Button
+        label="변경하기"
+        full
+        className={$['change-btn']}
+        disabled={buttonDisabled}
+        onClick={() => changePassword(password)}
+      />
     </Body>
   )
 }
